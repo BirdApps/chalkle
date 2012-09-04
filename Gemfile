eb44160fbf96f62f9de75c1adae1d5aab1600341
Joshua Vial <joshua@enspiral.com>
2012-09-04 23:54:54 +1200

    working on core models

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.6'
 gem 'pg'
 
 gem 'devise'
+gem 'rails_admin'
 gem 'html5-rails'
 gem 'haml-rails'
 gem 'simple_form'
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,3 @@
 class Lesson < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :title, :body, :category_id, :kind, :title, :doing, :learn, :skill, :skill_note, :teacher_name, :teacher_qualification, :bring, :charge, :cost, :book, :note, :start, :end, :link
 end
