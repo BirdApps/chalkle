e433bc4effa47013c1605748370b09ac6b3fcb04
Kiesia Croucher <kiesia@be.autiful.net>
2012-10-24 22:02:37 +1300

    added self.down method to groups migration

--- db/migrate/20121024034355_create_groups.rb
+++ db/migrate/20121024034355_create_groups.rb
@@ -1,10 +1,14 @@
 class CreateGroups < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :groups do |t|
       t.string :name
       t.string :api_key
 
       t.timestamps
     end
+  end
+
+  def self.down
+    drop_table :groups
   end
 end
