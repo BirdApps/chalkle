9062200d4f337c52d7b77df18aa70da090ed5db1
Kiesia Croucher <kiesia@be.autiful.net>
2012-10-25 14:07:00 +1300

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121024034355) do
+ActiveRecord::Schema.define(:version => 20121024085045) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -89,6 +89,13 @@ ActiveRecord::Schema.define(:version => 20121024034355) do
     t.string   "unlock_token"
     t.datetime "locked_at"
   end
+
+  create_table "group_admins", :id => false, :force => true do |t|
+    t.integer "group_id",      :null => false
+    t.integer "admin_user_id", :null => false
+  end
+
+  add_index "group_admins", ["group_id", "admin_user_id"], :name => "index_group_admins_on_group_id_and_admin_user_id", :unique => true
 
   create_table "groups", :force => true do |t|
     t.string   "name"
