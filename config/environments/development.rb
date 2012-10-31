d6acf39ebe17fe713a287302bc6a01aec61cb3a5
Kiesia Croucher <kiesia@be.autiful.net>
2012-10-31 14:18:49 +1300

    added staging environment, added action_mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,9 @@ Chalkle::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  ActionMailer::Base.delivery_method = :file
+  ActionMailer::Base.file_settings = { location: Rails.root.join("tmp/mail") }
 end
