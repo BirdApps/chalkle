0dcafcaa984f9f49beadef6a4973f2b60103eaa0
Kiesia Croucher <kiesia@be.autiful.net>
2012-10-31 22:14:16 +1300

    added associations to AdminUser

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,14 +4,26 @@ class AdminUser < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :super, :email, :password, :password_confirmation, :remember_me, :group_ids
 
   has_many :group_admins
   has_many :groups, :through => :group_admins
+  has_many :lessons, :through => :groups
+  has_many :chalklers, :through => :groups
+  has_many :bookings, :through => :groups
+
+  scope :super, where(:super => true)
 
   after_create { |admin| admin.send_reset_password_instructions }
+  before_destroy :raise_if_last
 
   def password_required?
     new_record? ? false : super
+  end
+
+  def raise_if_last
+    if AdminUser.count < 2
+      raise "Can't delete last admin user"
+    end
   end
 end
