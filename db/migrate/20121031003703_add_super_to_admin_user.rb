3648399ed7569c005dbec6716716178b1e421528
Kiesia Croucher <kiesia@be.autiful.net>
2012-11-01 13:23:32 +1300

    removed admin_user.super, added admin_user.role migrations

--- db/migrate/20121031003703_add_super_to_admin_user.rb
+++
@@ -1,9 +0,0 @@
-class AddSuperToAdminUser < ActiveRecord::Migration
-  def self.up
-    add_column :admin_users, :super, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column :admin_users, :super
-  end
-end
