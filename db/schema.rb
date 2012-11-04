7d138219fdd91a8ad2904ffa08b084c6d16065ae
Kiesia Croucher <kiesia@be.autiful.net>
2012-11-05 01:09:31 +1300

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -126,7 +126,6 @@ ActiveRecord::Schema.define(:version => 20121101001605) do
     t.string   "api_key"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "urlname"
     t.string   "url_name"
   end
 
