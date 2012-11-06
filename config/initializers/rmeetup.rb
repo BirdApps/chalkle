0574f0fe198246c67dc74047e497068a70ad5b11
Kiesia Croucher <kiesia@be.autiful.net>
2012-11-06 17:14:09 +1300

    readded default meetup api key

--- config/initializers/rmeetup.rb
+++ config/initializers/rmeetup.rb
@@ -1,3 +1,3 @@
 RMeetup2::Base.authentication_method = :api_key
-# Wellingon
-# RMeetup2::Base.api_key = '7e4a52464f704b4e1ea2a43166b1410'
+# Joshua's key
+RMeetup2::Base.api_key = '7e4a52464f704b4e1ea2a43166b1410'
