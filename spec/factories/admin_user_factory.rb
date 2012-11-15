fe2be224bfab5d80a65b13233b76834e58294e08
Kiesia Croucher <kiesia@be.autiful.net>
2012-11-15 22:33:14 +1300

    minor tweak to factory

--- spec/factories/admin_user_factory.rb
+++ spec/factories/admin_user_factory.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :admin_user do
     name Faker::Name.name
     email Faker::Internet.email
-    role ["super", "group admin"].sample
+    role { ["super", "group admin"].sample }
     groups {[ FactoryGirl.create(:group) ]}
 
     factory :super_admin_user do
