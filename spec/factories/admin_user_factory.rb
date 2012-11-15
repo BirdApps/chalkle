70f9cadc05422bb5f82cac3ba8ae7410bc2e263c
Kiesia Croucher <kiesia@be.autiful.net>
2012-11-15 22:26:03 +1300

    updated factories

---
+++ spec/factories/admin_user_factory.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  factory :admin_user do
+    name Faker::Name.name
+    email Faker::Internet.email
+    role ["super", "group admin"].sample
+    groups {[ FactoryGirl.create(:group) ]}
+
+    factory :super_admin_user do
+      role "super"
+    end
+
+    factory :group_admin_user do
+      role "group admin"
+    end
+  end
+end
--- spec/factories/booking_factory.rb
+++ spec/factories/booking_factory.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :booking do
-    meetup_id 12345678
-    status "yes"
-    guests 0
-    paid nil
+    meetup_id { (0...8).map{ rand(10) }.join }
+    status { ["yes", "no"].sample }
+    guests { rand(5) }
+    paid { [true, false].sample }
     chalkler
     lesson
   end
