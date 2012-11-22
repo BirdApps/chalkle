f72362055a350573b11d9e3f0c61999885c09624
Kiesia Croucher <kiesia@be.autiful.net>
2012-11-22 17:02:07 +1300

    abilities 1000% more sensible

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,14 +4,14 @@ class Ability
   def initialize(user)
     user ||= AdminUser.new
     case user.role
-      when "super"
-        can :manage, :all
-      when "group admin"
-        can :manage, Chalkler, :groups => { :id => user.groups }
-        can :manage, Category, :groups => { :id => user.groups }
-        can :manage, Lesson, :groups => { :id => user.groups }
-        can :manage, Booking, :lesson => { :id => user.groups.collect(&:lessons).flatten }
-        can :manage, Payment, :booking => { :id => user.groups.collect(&:bookings).flatten }
-      end
+    when "super"
+      can :manage, :all
+    when "group admin"
+      can :manage, Chalkler, :id => user.chalkler_ids
+      can :manage, Category, :id => user.category_ids
+      can :manage, Booking, :id => user.booking_ids
+      can :manage, Payment, :id => user.payment_ids
+      can :manage, Lesson, :id => user.lesson_ids
+    end
   end
 end
