b0d01070b6223776e7dd707fa51ae71328fc1098
Kiesia Croucher <kiesia@be.autiful.net>
2012-11-27 20:09:58 +1300

    removed sortability from cost column

--- app/admin/bookings.rb
+++ app/admin/bookings.rb
@@ -24,7 +24,7 @@ ActiveAdmin.register Booking do
       booking.lesson.groups.collect{|g| g.name}.join(", ")
     end
     column :status
-    column :cost
+    column :cost, :sortable => false
     column :paid
     column :guests
     column :created_at
