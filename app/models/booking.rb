0ceb11fdc1b96483d3fb8a8e68957fa86aa11939
Kiesia Croucher <kiesia@be.autiful.net>
2012-11-27 17:58:05 +1300

    added debugging code to booking import

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -10,7 +10,7 @@ class Booking < ActiveRecord::Base
   scope :confirmed, where(status: "yes")
 
   validates_uniqueness_of :chalkler_id, scope: :lesson_id
-  validates_uniqueness_of :meetup_id, allow_nil: true
+  # validates_uniqueness_of :meetup_id, allow_nil: true
   validates_presence_of :lesson_id
   validates_presence_of :chalkler_id
 
