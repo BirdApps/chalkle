c9282debfa2a8edeb54671678f9493ac8e590cf8
Kiesia Croucher <kiesia@be.autiful.net>
2012-11-28 14:59:15 +1300

    added booking tests

--- spec/factories/booking_factory.rb
+++ spec/factories/booking_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :booking do
     meetup_id { (0...8).map{ rand(10) }.join }
-    status { ["yes", "no"].sample }
+    status { ["yes", "no", "waitlist"].sample }
     guests { rand(5) }
     paid { [true, false].sample }
     chalkler
