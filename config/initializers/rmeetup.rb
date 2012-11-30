fba86f71f3aec6beeaaf574f9224e986bcf7285e
Kiesia Croucher <kiesia@be.autiful.net>
2012-11-30 19:46:25 +1300

    monkeypatched rMeetup to send better headers

--- config/initializers/rmeetup.rb
+++ config/initializers/rmeetup.rb
@@ -1,1 +1,12 @@
 RMeetup::Client.api_key = '7e4a52464f704b4e1ea2a43166b1410'
+
+RMeetup::Fetcher::Base.module_eval do
+  def get_response(url)
+    uri = URI.parse(url)
+    req = Net::HTTP::Get.new(uri.request_uri)
+    req["Accept-Charset"] = "utf-8"
+
+    res = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(req) }
+    res.body || raise(NoResponseError.new)
+  end
+end
