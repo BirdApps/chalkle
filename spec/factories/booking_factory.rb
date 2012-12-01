74f71189f129bf787ef733a1d826f4de26859a53
Kiesia Croucher <kiesia@be.autiful.net>
2012-12-01 20:28:54 +1300

    de-stupidified booking factory

--- spec/factories/booking_factory.rb
+++ spec/factories/booking_factory.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :booking do
-    meetup_id { (0...8).map{ rand(10) }.join }
-    status { ["yes", "no", "waitlist"].sample }
-    guests { rand(5) }
-    paid { [true, false].sample }
+    meetup_id 12345678
+    status "yes"
+    guests 3
+    paid false
     chalkler
     lesson
   end
