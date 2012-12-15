b18152e5a1c05b8e4c7ee4cf7f9a2b827c17935a
Amy Peng <amywpeng@gmail.com>
2012-12-15 05:24:53 +0100

    tests for visible scopes

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,4 +1,30 @@
 require 'spec_helper'
 
 describe Payment do
+  it { should validate_uniqueness_of(:xero_id) }
+
+  describe ".show_visible_only" do
+    context "show visible only should include visible payment" do
+      let(:payment) { FactoryGirl.create(:payment, visible: true) }
+      it { Payment.show_visible_only.should include(payment) }
+   	end
+
+   	context "show visible only should not include invisible payment" do
+      let(:payment) { FactoryGirl.create(:payment, visible: false) }
+      it { Payment.show_visible_only.should_not include(payment) }
+   	end
+
+  describe ".show_invisible_only" do
+    context "show invisible only should include invisible payment" do
+      let(:payment) { FactoryGirl.create(:payment, visible: false) }
+      it { Payment.show_invisible_only.should include(payment) }
+   	end
+
+   	context "show invisible only should not include visible payment" do
+      let(:payment) { FactoryGirl.create(:payment, visible: true) }
+      it { Payment.show_invisible_only.should_not include(payment) }
+   	end
+
+  end
+
 end
