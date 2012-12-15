5bc514de6800f13ef24f0f0a8a665ee712749645
Amy Peng <amywpeng@gmail.com>
2012-12-15 06:57:56 +0100

    test for visible scope

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -2,4 +2,28 @@ require 'spec_helper'
 
 describe Lesson do
   it { should validate_uniqueness_of :meetup_id }
+
+  describe ".show_visible_only" do
+    context "show visible only should include visible lesson" do
+      let(:lesson) { FactoryGirl.create(:lesson, visible: true) }
+      it { Lesson.show_visible_only.should include(lesson) }
+   	end
+
+   	context "show visible only should not include invisible lesson" do
+      let(:lesson) { FactoryGirl.create(:lesson, visible: false) }
+      it { Lesson.show_visible_only.should_not include(lesson) }
+  end
+
+  describe ".show_invisible_only" do
+    context "show invisible only should include invisible lesson" do
+      let(:lesson) { FactoryGirl.create(:lesson, visible: false) }
+      it { Lesson.show_invisible_only.should include(lesson) }
+   	end
+
+   	context "show invisible only should not include visible lesson" do
+      let(:lesson) { FactoryGirl.create(:lesson, visible: true) }
+      it { Lesson.show_invisible_only.should_not include(lesson) }
+   	end
+  end
+
 end
