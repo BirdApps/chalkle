1163d02711975022cddace913c9ee22fd7e09613
Kiesia Croucher <kiesia@be.autiful.net>
2012-12-18 19:42:50 +1300

    improved lesson spec

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -3,28 +3,25 @@ require 'spec_helper'
 describe Lesson do
   it { should validate_uniqueness_of :meetup_id }
 
+  let(:lesson) { FactoryGirl.create(:lesson) }
+
   describe ".visible" do
-    it "should include visible lesson" do
-      lesson = FactoryGirl.create(:lesson, visible: true) 
-      Lesson.visible.should include(lesson) 
-   	end
+    it { Lesson.visible.should include(lesson) }
 
    	it "should not include hidden lesson" do
-      lesson = FactoryGirl.create(:lesson, visible: false) 
+      lesson.visible = false
+      lesson.save
       Lesson.visible.should_not include(lesson)
     end
   end
 
   describe ".hidden" do
-   it "should include hidden lesson" do
-      lesson = FactoryGirl.create(:lesson, visible: false) 
+    it "should include hidden lesson" do
+      lesson.visible = false
+      lesson.save
       Lesson.hidden.should include(lesson)
    	end
 
-   	it "should not include visible lesson" do
-      lesson = FactoryGirl.create(:lesson, visible: true) 
-      Lesson.hidden.should_not include(lesson)
-   	end
+    it { Lesson.hidden.should_not include(lesson) }
   end
-
 end
