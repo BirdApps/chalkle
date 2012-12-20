e475849ab6e0b8a381efd11f246d2f16c420ec53
Kiesia Croucher <kiesia@be.autiful.net>
2012-12-20 16:18:44 +1300

    made additional cost reversable

--- db/migrate/20121215065642_add_additional_cost_to_booking.rb
+++ db/migrate/20121215065642_add_additional_cost_to_booking.rb
@@ -1,5 +1,9 @@
 class AddAdditionalCostToBooking < ActiveRecord::Migration
-  def change
+  def up
     add_column :bookings, :additional_cost, :decimal, :default => 0, :precision => 8, :scale => 2
+  end
+
+  def down
+    remove_column :bookings, :additional_cost
   end
 end
