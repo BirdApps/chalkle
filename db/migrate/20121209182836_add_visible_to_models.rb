c557554ee51b8133f9a5f53f5e527a5ebe2ca8bf
Kiesia Croucher <kiesia@be.autiful.net>
2013-01-04 23:28:07 +1300

    fixed failing migration

--- db/migrate/20121209182836_add_visible_to_models.rb
+++ db/migrate/20121209182836_add_visible_to_models.rb
@@ -4,9 +4,13 @@ class AddVisibleToModels < ActiveRecord::Migration
     add_column :bookings, :visible, :boolean
     add_column :lessons, :visible, :boolean
 
-    Payments.all.each { |p| p.update_attribute visible, true }
-    Booking.all.each { |p| p.update_attribute visible, true }
-    Lesson.all.each { |p| p.update_attribute visible, true }
+    Payment.reset_column_information
+    Booking.reset_column_information
+    Lesson.reset_column_information
+
+    Payment.all.each { |p| p.update_attribute :visible, true }
+    Booking.all.each { |b| b.update_attribute :visible, true }
+    Lesson.all.each { |l| l.update_attribute :visible, true }
   end
 
   def down
