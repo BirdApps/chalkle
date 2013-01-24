5ab386acb3a36792fde4d52e2e057ddf19fd79eb
amypeng <amywpeng@gmail.com>
2013-01-24 03:37:55 +0000

    change cost override to allow 0

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -40,7 +40,7 @@ class Booking < ActiveRecord::Base
   end
 
   def cost
-    return cost_override if cost_override?
+    return cost_override if (cost_override? || cost_override===0)
     seats = guests.present? ? guests + 1 : 1
     lesson.cost.present? ? (lesson.cost * seats) : nil
   end
