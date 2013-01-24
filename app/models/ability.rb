d3fdd6ef3bc3894abcb32462ef53e434e3c60e41
amypeng <amywpeng@gmail.com>
2013-01-24 08:24:03 +0000

    give group admin ability to register cash payments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,7 @@ class Ability
       can :manage, Group
       can [:read, :create, :update], Chalkler
       can [:read, :create, :update, :hide, :unhide], [Booking, Payment]
+      can [:record_cash_payment], Booking
       can [:reconcile, :do_reconcile, :download_from_xero], Payment
       can [:read, :update, :hide, :unhide], Lesson
       can :manage, Category
@@ -16,6 +17,7 @@ class Ability
     when "group admin"
       can :read, Chalkler, :id => user.chalkler_ids
       can [:read, :update], Lesson, :id => user.lesson_ids
+      can [:record_cash_payment], Booking
       cannot [:read, :update, :destroy], [Payment, Booking, Category, AdminUser, Group]
     end
   end
