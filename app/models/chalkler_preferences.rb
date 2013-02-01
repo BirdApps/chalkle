7b3c2ec871850a5205084c4d30b4ac64217bbdc9
Samson Ootoowak <sam@enspiral.com>
2013-02-01 14:36:10 +1300

    Save updated attributes.

--- app/models/chalkler_preferences.rb
+++ app/models/chalkler_preferences.rb
@@ -1,11 +1,28 @@
 class ChalklerPreferences
   include ActiveAttr::Model
 
-  attr_accessor :email, :email_frequency, :email_categories
+  attr_accessor :chalkler, :email, :email_frequency, :email_categories
 
   def initialize(chalkler)
-    @email = chalkler.email
-    @email_frequency = chalkler.email_frequency
-    @email_categories = chalkler.email_categories
+    @chalkler         = chalkler
+    @email            = @chalkler.email
+    @email_frequency  = @chalkler.email_frequency
+    @email_categories = @chalkler.email_categories
+  end
+
+  def update_attributes(params)
+    @email            = params[:email]
+    @email_frequency  = params[:email_frequency]
+    @email_categories = params[:email_categories]
+    persist_to_chalkler
+  end
+
+  private
+
+  def persist_to_chalkler
+    @chalkler.email            = @email
+    @chalkler.email_frequency  = @email_frequency
+    @chalkler.email_categories = @email_categories
+    @chalkler.save
   end
 end
--- spec/models/chalkler_preferences_spec.rb
+++ spec/models/chalkler_preferences_spec.rb
@@ -2,20 +2,67 @@ require 'spec_helper'
 
 describe ChalklerPreferences do
   let(:chalkler) { double('chalkler', email: 'test@user.com', email_frequency: 'weekly', email_categories: [1]) }
+  let(:params) { { email: 'tested@user.com', email_frequency: 'none', email_categories: [2] } }
 
-  describe "#initialize" do
+  describe '#initialize' do
     before { @email_prefs = ChalklerPreferences.new(chalkler) }
 
-    it "extracts an email" do
+    it 'extracts an email' do
       @email_prefs.email.should eq('test@user.com')
     end
 
-    it "extracts an email_frequency" do
+    it 'extracts an email_frequency' do
       @email_prefs.email_frequency.should eq('weekly')
     end
 
-    it "extracts the eamil categories" do
+    it 'extracts the eamil categories' do
       @email_prefs.email_categories.should eq([1])
+    end
+  end
+
+  describe '#update_attributes' do
+    before do
+      @new_chalkler = FactoryGirl.build(:chalkler)
+      @email_prefs = ChalklerPreferences.new(@new_chalkler)
+    end
+
+    context 'saving' do
+      it 'returns true for valid params' do
+        @email_prefs.update_attributes(params).should be_true
+      end
+
+      it 'returns false for invalid params' do
+        params.delete(:email)
+        @email_prefs.update_attributes(params).should be_true
+      end
+    end
+
+    context 'parameters' do
+      before { @email_prefs.update_attributes(params) }
+
+      it 'extracts an email' do
+        @email_prefs.email.should eq('tested@user.com')
+      end
+
+      it 'extracts an email_frequency' do
+        @email_prefs.email_frequency.should eq('none')
+      end
+
+      it 'extracts the eamil categories' do
+        @email_prefs.email_categories.should eq([2])
+      end
+
+      it 'extracts an email' do
+        @new_chalkler.email.should eq('tested@user.com')
+      end
+
+      it 'extracts an email_frequency' do
+        @new_chalkler.email_frequency.should eq('none')
+      end
+
+      it 'extracts the eamil categories' do
+        @new_chalkler.email_categories.should eq([2])
+      end
     end
   end
 end
