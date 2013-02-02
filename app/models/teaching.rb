bba22fcc6d4379034ed474252b58d36d71abb02b
amypeng <amywpeng@gmail.com>
2013-02-02 04:20:40 +0000

    change self to symbols

--- app/models/teaching.rb
+++ app/models/teaching.rb
@@ -5,8 +5,9 @@ class Teaching
   :suggested_dates, :prerequisites, :anything_else
 
   validates :title, :presence => { :message => "What we will do during the class can not be blank"}
+  validates :teacher_id, :presence => { :message => "You must be registered with chalkle first"}
   validates :do_during_class, :presence => { :message => "What we will do during the class can not be blank"}
-  validates :learning_outcomes, :presence => { :message => "What we will learn from the class can not be blank"}
+  validates :learning_outcomes, :presence => { :message => "What we will learn from this class can not be blank"}
   validates :duration, :allow_blank => true, :numericality => { :greater_than => 0, :message => "Only numbers greater than 0 should be entered here"}
   validates :teacher_cost, :allow_blank => true, :numericality => {:equal_to => 0, :message => "You can not be paid for a free class" }, :if => "self.free_lesson=='1'"
   validates :teacher_cost, :allow_blank => true, :numericality => {:greater_than_or_equal_to => 0, :message => "Only positive currencies are allowed" }
@@ -15,31 +16,33 @@ class Teaching
 
   def initialize(chalkler)
   	@chalkler = chalkler
-  	@teacher_id = @chalkler.id
-  	@bio = @chalkler.bio
+    @teacher_id = @chalkler.id
+    @bio = @chalkler.bio
   end
 
   def check_valid_input(params)
-    self.title = params[:title]
-    self.do_during_class = params[:do_during_class]
-    self.learning_outcomes = params[:learning_outcomes]
-    self.duration = params[:duration]
-    self.teacher_cost = params[:teacher_cost]
-    self.free_lesson = params[:free_lesson]
-    self.max_attendee = params[:max_attendee]
-    self.min_attendee = params[:min_attendee]
+    @title = params[:title]
+    @do_during_class = params[:do_during_class]
+    @learning_outcomes = params[:learning_outcomes]
+    @duration = params[:duration]
+    @teacher_cost = params[:teacher_cost]
+    @free_lesson = params[:free_lesson]
+    @max_attendee = params[:max_attendee]
+    @min_attendee = params[:min_attendee]
     self.valid?
   end
 
   def submit()
-  	@lesson = Lesson.new(name: self.title, teacher_id: @teacher_id, duration: self.duration.to_i*60, cost: price_calculation(self.teacher_cost), 
-      teacher_cost: self.teacher_cost.to_d)
-  	@lesson.save
+    @lesson = Lesson.new(name: @title, teacher_id: @teacher_id, duration: @duration.to_i*60, cost: price_calculation(self.teacher_cost), 
+      teacher_cost: @teacher_cost)
+    @lesson.save
   end
 
   def price_calculation(teacher_cost)
     (teacher_cost.blank? ? 0: teacher_cost.to_d*1.20)
   end
+
+  
 
 
 end
