52302f4d82d510b3f0dc3fb6f78f75a48ce7d9c1
amypeng <amywpeng@gmail.com>
2013-02-02 08:12:59 +0000

    add more columns to lesson table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130202064111) do
+ActiveRecord::Schema.define(:version => 20130202075631) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -152,6 +152,8 @@ ActiveRecord::Schema.define(:version => 20130202064111) do
     t.boolean  "visible"
     t.decimal  "teacher_payment", :precision => 8, :scale => 2
     t.string   "lesson_type"
+    t.text     "teacher_bio"
+    t.text     "do_during_class"
   end
 
   create_table "payments", :force => true do |t|
