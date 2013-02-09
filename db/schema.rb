ddf94e8ed18665987e986a87503319f23e819311
amypeng <amywpeng@gmail.com>
2013-02-09 21:14:02 +0000

    scheme change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130207224548) do
+ActiveRecord::Schema.define(:version => 20130209205231) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
