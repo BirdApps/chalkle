3651a5d1c94acc50e56529fc32fe66b393b16774
amypeng <amywpeng@gmail.com>
2013-02-10 09:33:09 +0000

    modify ordering of channels

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -3,9 +3,7 @@ class Channel < ActiveRecord::Base
 
   has_many :lessons
 
-  #TODO: Move into a presenter class like Draper sometime
-  #FIXME: Data should be inforced in some convention so lowercase conversion is not required here (probably all lowercase)
   def self.select_options
-    all(order: "LOWER(name)").map { |c| [c.name.titleize, c.id] }
+    all(order: "name").map { |c| [c.name, c.id] }
   end
 end
