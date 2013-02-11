3880a718c98a4c7192218025afcf24cb146e665c
amypeng <amywpeng@gmail.com>
2013-02-11 08:32:24 +0000

    get rid of settings

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Chalkle::Application.routes.draw do
     root to: "dashboard#index"
     match '/classes' => 'dashboard#classes', as: 'classes'
     resources :teachings
-    get '/settings' => 'preferences#show', as: 'preferences'
-    put '/settings' => 'preferences#save', as: 'preferences'
+    get '/preferences' => 'preferences#show', as: 'preferences'
+    put '/preferences' => 'preferences#save', as: 'preferences'
   end
 
   match '/image' => 'image#generate'
