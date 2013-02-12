e37c344274156111edfa80ffba2584d14bb04622
amypeng <amywpeng@gmail.com>
2013-02-12 23:00:53 +0000

    give everyone access to meetup template

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,12 +11,12 @@ class Ability
       can [:read, :create, :update, :hide, :unhide], [Booking, Payment]
       can [:record_cash_payment], Booking
       can [:reconcile, :do_reconcile, :download_from_xero, :unreconcile], Payment
-      can [:read, :update, :hide, :unhide, :lesson_email, :payment_summary_email], Lesson
+      can [:read, :update, :hide, :unhide, :lesson_email, :payment_summary_email, :meetup_template], Lesson
       can :manage, Category
 
     when "group admin"
       can :read, Chalkler, :id => user.chalkler_ids
-      can [:read, :update], Lesson, :id => user.lesson_ids
+      can [:read, :update, :meetup_template], Lesson, :id => user.lesson_ids
       can [:record_cash_payment], Booking
       cannot [:read, :update, :destroy], [Payment, Booking, Category, AdminUser, Group]
     end
