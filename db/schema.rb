7eeb54059015e7381cc4cc85af0855b5acd19f0b
amypeng <amywpeng@gmail.com>
2013-02-14 07:36:11 +0000

    replace categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130214004456) do
+ActiveRecord::Schema.define(:version => 20130214071857) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -130,10 +130,9 @@ ActiveRecord::Schema.define(:version => 20130214004456) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "url_name"
-    t.string   "bank_account_id"
   end
 
   create_table "lessons", :force => true do |t|
