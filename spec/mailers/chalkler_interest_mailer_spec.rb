2e292b123ed853a0dca7d536840d0d1a5a574e8c
amypeng <amywpeng@gmail.com>
2013-02-15 02:07:59 +0000

    daily email content complete. For lessons with single category only

--- spec/mailers/chalkler_interest_mailer_spec.rb
+++ spec/mailers/chalkler_interest_mailer_spec.rb
@@ -1,5 +1,13 @@
 require "spec_helper"
 
 describe ChalklerInterestMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include EmailSpec::Helpers
+  include EmailSpec::Matchers
+
+  describe "Daily digest mail content" do
+  	let(:lesson) { FactoryGirl.create(:lesson, cost: 10) }
+    let(:chalkler) { FactoryGirl.create(:chalkler) }
+    
+
+  end
 end
