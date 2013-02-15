5b98e01679b18c17ca427ee512f64f07cb2b4981
amypeng <amywpeng@gmail.com>
2013-02-15 02:18:17 +0000

    combine digest frequencies into one function

--- app/mailers/chalkler_interest_mailer.rb
+++ app/mailers/chalkler_interest_mailer.rb
@@ -1,21 +1,12 @@
 class ChalklerInterestMailer < ActionMailer::Base
   default from: "learn@chalkle.com"
 
-  def daily_digest(chalkler,new_lessons,still_open_lessons) 
-  	#take in name of the person to send email to and new lessons in the last 24 hours
+  def digest(chalkler,new_lessons,still_open_lessons,frequency) 
   	@chalkler = chalkler
   	@new_lessons = new_lessons
   	@still_open_lessons = still_open_lessons
-  	mail(to: chalkler.email, subject: chalkler.name + " - Here is your daily digest for " + Date.today().to_s)
+  	@frequency = frequency
+  	mail(to: chalkler.email, subject: chalkler.name + " - Here is your " + @frequency.to_s + " digest for " + Date.today().to_s)
   end
-
-  def weekly_digest(chalkler,new_lessons,still_open_lessons) 
-  	#take in name of the person to send email to and new lessons in the last week
-  	@chalkler = chalkler
-  	@new_lessons = new_lessons
-  	@still_open_lessons = still_open_lessons
-  	mail(to: chalkler.email, subject: chalkler.name + " - Here is your weekly digest for " + Date.today().to_s)
-  end
-
 
 end
