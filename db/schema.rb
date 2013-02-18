0ca3f90497f21531c5958f93045e58cf84386a51
amypeng <amywpeng@gmail.com>
2013-02-18 07:21:06 +0000

    teacher bio is a text not string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130217032437) do
+ActiveRecord::Schema.define(:version => 20130218071437) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -153,7 +153,7 @@ ActiveRecord::Schema.define(:version => 20130217032437) do
     t.boolean  "visible"
     t.decimal  "teacher_payment",     :precision => 8, :scale => 2
     t.string   "lesson_type"
-    t.string   "teacher_bio"
+    t.text     "teacher_bio"
     t.text     "do_during_class"
     t.text     "learning_outcomes"
     t.integer  "max_attendee"
