74f4168f0d25045f26bcd30119031edf674707ef
Kiesia Croucher <kiesia@be.autiful.net>
2013-02-25 12:02:16 +1300

    migrate category data

--- db/schema.rb
+++ db/schema.rb
@@ -100,22 +100,6 @@ ActiveRecord::Schema.define(:version => 20130219132647) do
     t.text     "email_streams"
   end
 
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
-    t.text     "handler"
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
-
   create_table "channel_admins", :id => false, :force => true do |t|
     t.integer "channel_id",    :null => false
     t.integer "admin_user_id", :null => false
@@ -146,10 +130,9 @@ ActiveRecord::Schema.define(:version => 20130219132647) do
 
   create_table "channels", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "url_name"
-    t.string   "bank_account_id"
   end
 
   create_table "lesson_categories", :id => false, :force => true do |t|
