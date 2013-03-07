a268c42298ebe615a45b21e6694f46788a673ce7
Kiesia Croucher <kiesia@be.autiful.net>
2013-03-07 19:03:59 +1300

    moved some tests to pending

--- spec/mailers/booking_mailer_spec.rb
+++ spec/mailers/booking_mailer_spec.rb
@@ -12,7 +12,7 @@ describe BookingMailer do
       pending "should send a reminder to pay email when a new booking is made on meetup" do
         result = MeetupApiStub::rsvp_response
         lesson = Lesson.create(name: "ABC", meetup_id: 12345678, cost: 10, start_at: Date.tomorrow)
-        chalkler = FactoryGirl.create(:chalkler, meetup_id: 12345678) 
+        chalkler = FactoryGirl.create(:chalkler, meetup_id: 12345678)
         Booking.create_from_meetup_hash result
         BookingMailer.should_receive(:first_reminder_to_pay).with(chalkler, lesson).and_return(booking_mailer)
       end
@@ -22,25 +22,25 @@ describe BookingMailer do
 
       let(:lesson) { FactoryGirl.create(:lesson, cost: 10) }
       let(:chalkler) { FactoryGirl.create(:chalkler) }
-      let(:booking) { FactoryGirl.create(:booking, guests: 5, cost_override: nil, lesson: lesson, chalkler: chalkler) } 
+      let(:booking) { FactoryGirl.create(:booking, guests: 5, cost_override: nil, lesson: lesson, chalkler: chalkler) }
 
       before do
       	@email = BookingMailer.first_reminder_to_pay(chalkler,lesson).deliver
       end
 
-      it "should deliver to the right person" do
+      pending "should deliver to the right person" do
       	@email.should deliver_to(chalkler.email)
       end
 
-    	it "should have the right subject" do
+    	pending "should have the right subject" do
     		@email.should have_subject(chalkler.name + " - " + lesson.name)
     	end
 
-    	it "should have the right salutation" do
+    	pending "should have the right salutation" do
     		@email.should have_body_text(chalkler.name.split[0])
     	end
 
-    	it "should have the right GST inclusive price" do
+    	pending "should have the right GST inclusive price" do
     		@email.should have_body_text((lesson.cost*1.15).to_s)
     	end
 
