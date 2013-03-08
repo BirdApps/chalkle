34ee79c9af99e09014d43e8c7ecd022b462a1934
allansideas <allansideas@gmail.com>
2013-03-08 12:16:24 +0100

    better message

--- app/controllers/chalklers/teachings_controller.rb
+++ app/controllers/chalklers/teachings_controller.rb
@@ -6,13 +6,14 @@ class Chalklers::TeachingsController < Chalklers::BaseController
   def create
   	@chalkler_teaching = Teaching.new(current_chalkler)
   	if @chalkler_teaching.submit(params[:teaching])
-  	 redirect_to success_chalklers_teachings_url
+  	 redirect_to success_chalklers_teachings_url, notice: 'Your class has been saved.'
   	else
       render 'new'
   	end
   end
 
   def success
+    @curator_email = current_chalkler.channels.first.email
   end
 
   def show
