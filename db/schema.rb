8f2e4478965412541c9121ac3f747c43a4b3437a
amypeng <amywpeng@gmail.com>
2013-03-13 19:13:26 +0000

    after migration

--- db/schema.rb
+++ db/schema.rb
@@ -133,8 +133,6 @@ ActiveRecord::Schema.define(:version => 20130308102928) do
     t.datetime "created_at",                                                          :null => false
     t.datetime "updated_at",                                                          :null => false
     t.string   "url_name"
-    t.decimal  "channel_percentage", :precision => 8, :scale => 2, :default => 0.125
-    t.decimal  "teacher_percentage", :precision => 8, :scale => 2, :default => 0.75
     t.string   "email"
     t.decimal  "channel_percentage", :precision => 8, :scale => 2, :default => 0.125
     t.decimal  "teacher_percentage", :precision => 8, :scale => 2, :default => 0.75
