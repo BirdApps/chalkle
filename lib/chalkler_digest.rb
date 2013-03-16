2c0d8514231e892a0fac0fa4c5ba6ebfbff9f3c6
Kiesia Croucher <kiesia@be.autiful.net>
2013-03-16 22:18:20 +1300

    mailer rafactoring

--- lib/chalkler_digest.rb
+++ lib/chalkler_digest.rb
@@ -1,47 +1,60 @@
 class ChalklerDigest
+  def initialize(c)
+    @chalkler = c
+    @frequency = c.email_frequency
+    @date_offset = (c.email_frequency == 'daily') ? 1 : 7
+    @limit = (c.email_frequency == 'daily') ? 5 : 10
+  end
+
+  def create!
+    new = new_lessons
+    new = default_new_lessons if new.empty?
+    open = open_lessons
+    open = default_open_lessons if open.empty?
+    ChalklerMailer.delay.digest(@chalkler, new, open)
+  end
+
   def self.load_chalklers(frequency)
     Chalkler.where("email IS NOT NULL AND email_frequency = ?", frequency)
   end
 
-  def prepare_digest
+  private
+
+  def new_lessons
+    Lesson.joins(:categories).where("lessons.created_at > CURRENT_DATE - ? AND
+                                    lessons.created_at <= CURRENT_DATE AND
+                                    lessons.status = 'Published' AND
+                                    lessons.visible = true AND
+                                    lesson_categories.category_id IN (?)",
+                                    @date_offset, @chalkler.email_categories)
   end
 
-  def queue_digest!
+  def default_new_lessons
+    date_offset = (@frequency == 'daily') ? 1 : 7
+    Lesson.where("created_at > CURRENT_DATE - ? AND
+                 created_at <= CURRENT_DATE AND
+                 status = 'Published' AND
+                 visible = true",
+                 @date_offset).limit(@limit)
   end
 
-  def load_new_lessons
+  def open_lessons
+    Lesson.joins(:categories).where("lessons.start_at > CURRENT_DATE + 1 AND
+                                    lessons.start_at <= CURRENT_DATE + ? AND
+                                    lessons.created_at <= CURRENT_DATE - ? AND
+                                    lessons.status = 'Published' AND
+                                    lessons.visible = true AND
+                                    lesson_categories.category_id IN (?)",
+                                    @date_offset + 1, @date_offset, @chalkler.email_categories)
   end
 
-  def load_open_lessons
+  def default_open_lessons
+    Lesson.joins(:categories).where("lessons.start_at > CURRENT_DATE + 1 AND
+                                    lessons.start_at <= CURRENT_DATE + ? AND
+                                    lessons.created_at <= CURRENT_DATE - ? AND
+                                    lessons.status = 'Published' AND
+                                    lessons.visible = true",
+                                    @date_offset + 1, @date_offset).limit(@limit)
   end
 
-  def self.lesson_filter(chalkler,lessons)
-    filtered_lessons = lessons.find(:all, :conditions => ["category_id IN (?)", chalkler.email_categories] )
-    if filtered_lessons.count == 0
-      if chalkler.email_frequency == "daily"
-        return lessons.find(:all, :limit => 5)
-      elsif chalkler.email_frequency == "weekly"
-        return lessons.find(:all, :limit => 10)
-      end
-    else
-      return filtered_lessons
-    end
-  end
-
-  # Change created_at to published_at when migration on lesson table has been completed
-  def filtered_new_lessons
-    if self.email_frequency == "daily"
-      lesson_filter(self,Lesson.where("created_at > current_date - 1 AND created_at <= current_date AND status = 'Published' AND visible = true "))
-    elsif self.email_frequency == "weekly"
-      lesson_filter(self,Lesson.where("created_at > current_date - 7 AND created_at <= current_date AND status = 'Published' AND visible = true "))
-    end
-  end
-
-  def filtered_still_open_lessons
-    if self.email_frequency == "daily"
-      lesson_filter(self,Lesson.where("start_at > current_date + 1 AND start_at <= current_date + 2 AND status = 'Published' AND visible = true AND created_at <= current_date - 1"))
-    elsif self.email_frequency == "weekly"
-      lesson_filter(self,Lesson.where("start_at > current_date + 1 AND start_at <= current_date + 8 AND status = 'Published' AND visible = true AND created_at <= current_date - 7"))
-    end
-  end
 end
--- spec/lib/chalkler_digest.rb
+++ spec/lib/chalkler_digest.rb
@@ -2,6 +2,40 @@ require 'spec_helper'
 require 'chalkler_digest'
 
 describe ChalklerDigest do
+  describe "#initialize" do
+    let(:chalkler){ FactoryGirl.create(:chalkler, email_frequency: 'daily') }
+
+    it "sets @chalkler" do
+      digest = ChalklerDigest.new(chalkler)
+      digest.instance_eval{ @chalkler }.should == chalkler
+    end
+
+    it "sets @frequency" do
+      digest = ChalklerDigest.new(chalkler)
+      digest.instance_eval{ @frequency }.should == 'daily'
+    end
+  end
+
+  describe "new_lessons" do
+    let(:category) { FactoryGirl.create(:category) }
+    let(:chalkler) { FactoryGirl.create(:chalkler, email_categories: [category.id]) }
+
+    it "won't load a lesson that is unpublished" do
+      lesson = FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Approved')
+      lesson.categories << category
+      digest = ChalklerDigest.new(chalkler)
+      digest.new_lessons.should be_empty
+    end
+
+    it "loads a lesson that a chalkler is interested in" do
+      lesson = FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
+      lesson.categories << category
+      FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
+      digest = ChalklerDigest.new(chalkler)
+      digest.new_lessons.count.should == 1
+    end
+  end
+
   describe ".load_chalklers" do
     it "won't return a chalkler without an email address" do
       FactoryGirl.create(:chalkler, email: nil, email_frequency: 'weekly')
