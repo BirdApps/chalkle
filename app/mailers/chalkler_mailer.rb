e163c54630a5b5012f0e69c2ddf4da514de1b399
Kiesia Croucher <kiesia@be.autiful.net>
2013-03-18 21:43:19 +1300

    layout changes

--- app/mailers/chalkler_mailer.rb
+++ app/mailers/chalkler_mailer.rb
@@ -7,6 +7,9 @@ class ChalklerMailer < ActionMailer::Base
   	@new_lessons = new_lessons
   	@open_lessons = open_lessons
   	mail(to: chalkler.email,
-         subject: "chalkle° - #{chalkler.email_frequency.titleize} class digest for #{Date.today.to_formatted_s(:long)}")
+         subject: "chalkle° - #{chalkler.email_frequency.titleize} class digest for #{Date.today.to_formatted_s(:long)}") do |format|
+           format.html { render :layout => 'standard_mailer' }
+           format.text
+         end
   end
 end
