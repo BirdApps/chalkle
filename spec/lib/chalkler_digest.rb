83310d4789ce104a6eab06fe26804964df5bd5b1
Kiesia Croucher <kiesia@be.autiful.net>
2013-03-19 13:34:46 +1300

    added specs for mailer

--- spec/lib/chalkler_digest.rb
+++ spec/lib/chalkler_digest.rb
@@ -4,35 +4,120 @@ require 'chalkler_digest'
 describe ChalklerDigest do
   describe "#initialize" do
     let(:chalkler){ FactoryGirl.create(:chalkler, email_frequency: 'daily') }
+    let(:digest) { ChalklerDigest.new(chalkler) }
 
     it "sets @chalkler" do
-      digest = ChalklerDigest.new(chalkler)
       digest.instance_eval{ @chalkler }.should == chalkler
     end
 
     it "sets @frequency" do
-      digest = ChalklerDigest.new(chalkler)
       digest.instance_eval{ @frequency }.should == 'daily'
+    end
+
+    it "sets @limit" do
+      digest.instance_eval{ @limit }.should == 5
     end
   end
 
-  describe "new_lessons" do
-    let(:category) { FactoryGirl.create(:category) }
-    let(:chalkler) { FactoryGirl.create(:chalkler, email_categories: [category.id]) }
-
-    it "won't load a lesson that is unpublished" do
-      lesson = FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Approved')
-      lesson.categories << category
-      digest = ChalklerDigest.new(chalkler)
-      digest.instance_eval{ new_lessons }.should be_empty
+  describe "lesson selection" do
+    before do
+      @category = FactoryGirl.create(:category)
+      @chalkler = FactoryGirl.create(:chalkler, email_categories: [@category.id])
+      @channel = FactoryGirl.create(:channel)
+      @chalkler.channels << @channel
+      @digest = ChalklerDigest.new(@chalkler)
     end
 
-    it "loads a lesson that a chalkler is interested in" do
-      lesson = FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
-      lesson.categories << category
-      FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
-      digest = ChalklerDigest.new(chalkler)
-      digest.instance_eval{ new_lessons }.count.should == 1
+    describe "#new_lessons" do
+      it "loads a lesson that a chalkler is interested in" do
+        lesson = FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
+        lesson.categories << @category
+        lesson.channels << @channel
+        lesson1 =  FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
+        lesson1.channels << @channel
+        @digest.instance_eval{ new_lessons }.count.should == 1
+      end
+
+      it "loads a lessons from channels that chalkler belongs to" do
+        lesson = FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
+        lesson.categories << @category
+        lesson.channels << @channel
+        lesson1 =  FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
+        lesson1.categories << @category
+        lesson1.channels << FactoryGirl.create(:channel)
+        @digest.instance_eval{ new_lessons }.count.should == 1
+      end
+
+      pending "won't load a lesson that is X old" do
+      end
+
+      pending "won't load a lesson that is X new" do
+      end
+    end
+
+    describe "#default_new_lessons" do
+      it "loads a lessons from channels that chalkler belongs to" do
+        lesson = FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
+        lesson.channels << @channel
+        lesson1 =  FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
+        lesson1.channels << FactoryGirl.create(:channel)
+        @digest.instance_eval{ default_new_lessons }.count.should == 1
+      end
+
+      pending "won't load a lesson that is X old" do
+      end
+
+      pending "won't load a lesson that is X new" do
+      end
+    end
+
+    describe "open_lessons" do
+      it "loads a lesson that a chalkler is interested in" do
+        lesson = FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
+        lesson.categories << @category
+        lesson.channels << @channel
+        lesson1 =  FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
+        lesson1.channels << @channel
+        @digest.instance_eval{ new_lessons }.count.should == 1
+      end
+
+      it "loads a lessons from channels that chalkler belongs to" do
+        lesson = FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
+        lesson.categories << @category
+        lesson.channels << @channel
+        lesson1 =  FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
+        lesson1.categories << @category
+        lesson1.channels << FactoryGirl.create(:channel)
+        @digest.instance_eval{ new_lessons }.count.should == 1
+      end
+
+      pending "only loads lessons with open rsvps" do
+      end
+
+      pending "won't load a lesson that is X old" do
+      end
+
+      pending "won't load a lesson that is X new" do
+      end
+    end
+
+    describe "#default_open_lessons" do
+      it "loads a lessons from channels that chalkler belongs to" do
+        lesson = FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
+        lesson.channels << @channel
+        lesson1 =  FactoryGirl.create(:lesson, created_at: 1.day.ago, status: 'Published')
+        lesson1.channels << FactoryGirl.create(:channel)
+        @digest.instance_eval{ default_new_lessons }.count.should == 1
+      end
+
+      pending "only loads lessons with open rsvps" do
+      end
+
+      pending "won't load a lesson that is X old" do
+      end
+
+      pending "won't load a lesson that is X new" do
+      end
     end
   end
 
