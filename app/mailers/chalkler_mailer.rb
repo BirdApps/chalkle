9597ca3f8a846fe1fe7cb6b4ef9a4b98e23948e0
Kiesia Croucher <kiesia@be.autiful.net>
2013-03-24 17:50:54 +1300

    created teacher_welcome mail

--- app/mailers/chalkler_mailer.rb
+++ app/mailers/chalkler_mailer.rb
@@ -2,12 +2,20 @@
 class ChalklerMailer < ActionMailer::Base
   default from: "noreply@chalkle.com"
 
+  def teacher_welcome(chalkler)
+    @chalkler = chalkler
+    mail(to: @chalkler.email, subject: "Welcome to chalkle°!") do |format|
+      format.html { render :layout => 'standard_mailer' }
+      format.text
+    end
+  end
+
   def digest(chalkler, new_lessons, open_lessons)
   	@chalkler = chalkler
   	@new_lessons = new_lessons
   	@open_lessons = open_lessons
-  	mail(to: chalkler.email,
-         subject: "chalkle° - #{chalkler.email_frequency.titleize} digest for #{Date.today.to_formatted_s(:long)}") do |format|
+  	mail(to: @chalkler.email,
+         subject: "chalkle° - #{@chalkler.email_frequency.titleize} digest for #{Date.today.to_formatted_s(:long)}") do |format|
            format.html { render :layout => 'standard_mailer' }
            format.text
          end
