54e376bc67be141b52ccd96b13e4a42fe1b7f6aa
amypeng <amywpeng@gmail.com>
2013-04-04 05:58:50 +0000

    modified new classes panel

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -12,8 +12,8 @@ ActiveAdmin.register_page "Dashboard" do
     columns do
       column do
 
-        panel "Classes for review" do
-          table_for Lesson.accessible_by(current_ability).visible.unpublished.order("updated_at asc") do
+        panel "Unreviewed classes" do
+          table_for Lesson.accessible_by(current_ability).visible.unreviewed.order("updated_at asc") do
             column("Name") { |lesson| link_to(lesson.name, admin_lesson_path(lesson)) }
             column("Teacher") { |lesson| lesson.teacher.present? ? (link_to(lesson.teacher.name, admin_chalkler_path(lesson.teacher))) : "No Teacher" }
             column :categories do |lesson|
@@ -22,11 +22,19 @@ ActiveAdmin.register_page "Dashboard" do
             column :channels do |lesson|
               lesson.channels.collect{ |c| c.name}.join(", ")
             end
-            column("Last Update") { |lesson| lesson.updated_at.to_formatted_s(:long) }
-            column("Status") { |lesson| lesson.status }
+            column("Comments") { |lesson| (comment = ActiveAdmin::Comment.where{(resource_type.eq "Lesson") & (resource_id.eq lesson.id.to_s)}.order("created_at").last).nil? ? nil : comment.body }
             column("Price (incl GST)") { |lesson| number_to_currency lesson.gst_price }
           end
         end
+
+        panel "On-hold classes" do
+          render partial: "/admin/dashboard/lesson_panel", locals: {lessons: Lesson.accessible_by(current_ability).visible.on_hold.order("updated_at asc")}
+        end
+
+        panel "Approved classes" do
+          render partial: "/admin/dashboard/lesson_panel", locals: {lessons: Lesson.accessible_by(current_ability).visible.approved.order("updated_at asc")}
+        end
+        
 
         panel "Upcoming classes" do
           table_for Lesson.accessible_by(current_ability).visible.upcoming.order("start_at asc") do
