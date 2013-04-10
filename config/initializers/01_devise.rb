5302261fa15975fcfc518794c27b5f92b9fdabef
Kiesia Croucher <kiesia@be.autiful.net>
2013-04-11 00:52:47 +1200

    added name to devise email address

--- config/initializers/01_devise.rb
+++ config/initializers/01_devise.rb
@@ -1,10 +1,11 @@
+# encoding: UTF-8
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "no-reply@chalkle.com"
+  config.mailer_sender = '"chalkle°" <noreply@chalkle.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
