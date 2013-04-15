728bee163dec958aeaddc69bc9656a5e55dab57c
Kiesia Croucher <kiesia@be.autiful.net>
2013-04-16 11:55:56 +1200

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -141,9 +141,9 @@ ActiveRecord::Schema.define(:version => 20130414033233) do
     t.datetime "created_at",                                                          :null => false
     t.datetime "updated_at",                                                          :null => false
     t.string   "url_name"
+    t.string   "email"
     t.decimal  "channel_percentage", :precision => 8, :scale => 2, :default => 0.125
     t.decimal  "teacher_percentage", :precision => 8, :scale => 2, :default => 0.75
-    t.string   "email"
   end
 
   create_table "delayed_jobs", :force => true do |t|
