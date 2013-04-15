88258c933ed2e330ab79a8164419857ffa494d46
Kiesia Croucher <kiesia@be.autiful.net>
2013-04-15 16:54:01 +1200

    simplified migration

--- db/migrate/20130414033233_add_phone_number_to_chalkler.rb
+++ db/migrate/20130414033233_add_phone_number_to_chalkler.rb
@@ -1,13 +1,5 @@
 class AddPhoneNumberToChalkler < ActiveRecord::Migration
-  def up
-  	add_column :chalklers, :phone_number, :string, :default => nil
-
-  	Chalkler.reset_column_information
-
-    Chalkler.all.each { |l| l.update_attribute :phone_number, nil }
-  end
-
-  def down
-  	remove_column :chalklers, :phone_number
+  def change
+  	add_column :chalklers, :phone_number, :string
   end
 end
