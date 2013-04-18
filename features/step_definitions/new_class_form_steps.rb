a94d744a8f87b7339af62ff4da5d8a6bc61ba72e
amypeng <amywpeng@gmail.com>
2013-04-18 14:36:55 +1200

    update views

--- features/step_definitions/new_class_form_steps.rb
+++ features/step_definitions/new_class_form_steps.rb
@@ -12,7 +12,7 @@ When /^they enter new class details with channel$/ do
   fill_in 'teaching_do_during_class', with: 'learning new stuff'
   fill_in 'teaching_learning_outcomes', with: 'the new stuff'
   select 'Science', from: 'teaching_category_primary_id'
-  select 'Whanau', from: 'teaching_channel_id'
+  select 'Whanau', from: 'teaching_channel_select'
   click_button 'Submit my class'
 end
 
