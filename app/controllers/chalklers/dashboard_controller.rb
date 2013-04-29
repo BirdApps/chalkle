4fad4259ed046d77c3c01c2437f0caa99340ce15
Kiesia Croucher <kiesia@be.autiful.net>
2013-04-30 00:22:27 +1200

    block users without channels

--- app/controllers/chalklers/dashboard_controller.rb
+++ app/controllers/chalklers/dashboard_controller.rb
@@ -2,10 +2,10 @@ class Chalklers::DashboardController < Chalklers::BaseController
   def index
   end
 
-  def classes
-  end
-
   def settings
   	@chalkler_email_preferences = ChalklerPreferences.new(current_chalkler)
+  end
+
+  def missing_channel
   end
 end
