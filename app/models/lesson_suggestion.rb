b5be1ecf76225372800efd90ed9faa5f60ade014
Kiesia Croucher <kiesia@be.autiful.net>
2013-04-30 21:29:05 +1200

    lesson suggestion updates

--- app/models/lesson_suggestion.rb
+++ app/models/lesson_suggestion.rb
@@ -1,6 +1,7 @@
 class LessonSuggestion < ActiveRecord::Base
   attr_accessible :description, :name, :category_id, :join_channels
   attr_accessible :description, :name, :category_id, :join_channels, :chalkler_id, :channel_ids, :as => :admin
+
   attr_accessor :join_channels
 
   belongs_to :chalkler
@@ -12,12 +13,10 @@ class LessonSuggestion < ActiveRecord::Base
   validates_presence_of :join_channels, :on => :create
   validates_presence_of :channel_ids, :on => :update
 
-  after_create :create_channel_associations
-
   private
 
   def create_channel_associations
-    join_channels.reject!(&:empty?)
+    return unless join_channels.is_a?(Array)
     join_channels.each do |channel_id|
       self.channels << Channel.find(channel_id)
     end
--- spec/factories/lesson_suggestion_factory.rb
+++ spec/factories/lesson_suggestion_factory.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :lesson_suggestion do
     name "Suggesting fun"
     description "You should really learn, suggest it, it's fun!"
+    join_channels [1]
   end
 end
