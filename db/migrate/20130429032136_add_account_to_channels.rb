e0f6af7ebb5bcb38413f1460e9096898ec724a79
amypeng <amywpeng@gmail.com>
2013-04-30 15:42:13 +1200

    make new migration

--- db/migrate/20130429032136_add_account_to_channels.rb
+++
@@ -1,9 +0,0 @@
-class AddAccountToChannels < ActiveRecord::Migration
-  def up
-  	add_column :channels, :account, :string, :default => nil
-  end
-
-  def down
-  	remove_column :channels, :account
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130429032136) do
+ActiveRecord::Schema.define(:version => 20130430033930) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -137,6 +137,7 @@ ActiveRecord::Schema.define(:version => 20130429032136) do
     t.decimal  "channel_percentage", :precision => 8, :scale => 2, :default => 0.125
     t.decimal  "teacher_percentage", :precision => 8, :scale => 2, :default => 0.75
     t.string   "email"
+    t.string   "account"
   end
 
   create_table "delayed_jobs", :force => true do |t|
