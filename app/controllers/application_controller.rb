f5bf5de1e54a3b9348361fdb6eb3565f875ad149
Kiesia Croucher <kiesia@be.autiful.net>
2013-05-04 22:48:08 +1200

    styleguide fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  #before_filter :authenticate, only: [:styleguide]
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to admin_dashboard_path, :alert => exception.message
@@ -12,12 +11,5 @@ class ApplicationController < ActionController::Base
 
   def styleguide
     render "/styleguide"
-  end
-
-  protected
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == "chalkle" && password == "chalkleadmin123"
-    end
   end
 end
