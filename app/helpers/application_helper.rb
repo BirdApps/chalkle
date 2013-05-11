53b67c35d0483f3f31bd0d0410fe08c7afdc69c0
Kiesia Croucher <kiesia@be.autiful.net>
2013-05-11 17:42:40 +1200

    added body_class helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def title(page_title)
     content_for :title, "#{page_title} | chalkle°"
   end
+
+  def body_class
+    [controller_name, action_name].join('-')
+  end
 end
