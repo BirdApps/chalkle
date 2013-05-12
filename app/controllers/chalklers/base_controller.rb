be8177c6c4951ebf81ad9895c1f912d7b4d92349
Kiesia Croucher <kiesia@be.autiful.net>
2013-05-13 11:24:42 +1200

    formatting

--- app/controllers/chalklers/base_controller.rb
+++ app/controllers/chalklers/base_controller.rb
@@ -1,6 +1,6 @@
 class Chalklers::BaseController < ApplicationController
   before_filter :authenticate_chalkler!
-  before_filter :has_channel_membership?, :except => [ :login, :missing_channel ]
+  before_filter :has_channel_membership?, :except => [:login, :missing_channel]
 
   protected
 
