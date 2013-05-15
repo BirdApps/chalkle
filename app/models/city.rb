7b9cc87b10b9cf1861a576bd47e8ce5e9e12b0f0
Kiesia Carmine <kiesia@be.autiful.net>
2013-05-15 16:26:24 +1200

    created City

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,8 +1,6 @@
 class City < ActiveRecord::Base
   attr_accessible :name, :as => :admin
 
-  has_many :cities_channels
-  has_many :channels, :through => :cities_channels
   has_many :venues
 
   validates_presence_of :name
