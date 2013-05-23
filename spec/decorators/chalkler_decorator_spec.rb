90f83c2841021c1ed232b421bf4e066d8997ab75
Kiesia Carmine <kiesia@be.autiful.net>
2013-05-23 13:35:02 +1200

    fixed spec

--- spec/decorators/chalkler_decorator_spec.rb
+++ spec/decorators/chalkler_decorator_spec.rb
@@ -4,17 +4,20 @@ describe ChalklerDecorator do
   describe ".channel_links" do
     before do
       @chalkler = FactoryGirl.create(:chalkler)
-      @chalkler.channels << FactoryGirl.create(:channel, name: 'xyz', url_name: 'xyz')
-      @chalkler.channels << @channel = FactoryGirl.create(:channel, name: 'xyz', url_name: '')
+      @chalkler.channels << FactoryGirl.create(:channel, name: 'Meetup', url_name: 'meetup')
+      @chalkler.channels << @channel = FactoryGirl.create(:channel, name: 'Local', url_name: '')
     end
 
-    it "creates a linked list of channels" do
-      @chalkler.decorate.channel_links.should == "<a href=\"http://www.meetup.com/xyz/events/calendar/?scroll=true\" style=\"\">xyz</a> | <a href=\"http://localhost:3000/channels/#{@channel.id}\" style=\"\">xyz</a>"
+    it "links to a Meetup based channel" do
+      @chalkler.decorate.channel_links.should include('Meetup', 'meetup')
+    end
+
+    it "links to a local channel" do
+      @chalkler.decorate.channel_links.should include('Local', "channels/#{@channel.id}")
     end
 
     it 'adds custom css' do
-      @chalkler.decorate.channel_links('color: #000;').should == "<a href=\"http://www.meetup.com/xyz/events/calendar/?scroll=true\" style=\"color: #000;\">xyz</a> | <a href=\"http://localhost:3000/channels/#{@channel.id}\" style=\"color: #000;\">xyz</a>"
+      @chalkler.decorate.channel_links('color: #000;').should include('color: #000;')
     end
-
   end
 end
