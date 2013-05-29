77eff4f2d7e485adee7ef28696d1da64b2b21f7e
amypeng <amywpeng@gmail.com>
2013-05-29 17:29:48 +1200

    remove invisible channels from the footer

--- app/decorators/chalkler_decorator.rb
+++ app/decorators/chalkler_decorator.rb
@@ -2,13 +2,13 @@ class ChalklerDecorator < Draper::Decorator
   delegate_all
 
   def channel_links(style = '')
-    source.channels.collect { |c|
-      if c.url_name?
-        url = "http://www.meetup.com/#{c.url_name}/events/calendar/?scroll=true"
-      else
-        url = h.channel_url(c)
-      end
-      h.link_to(c.name, url, { style: style })
+    source.channels.visible.collect { |c|
+        if c.url_name?
+          url = "http://www.meetup.com/#{c.url_name}/events/calendar/?scroll=true"
+        elsif
+          url = h.channel_url(c)
+        end
+        h.link_to(c.name, url, { style: style })
     }.join(' | ')
   end
 
