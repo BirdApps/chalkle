7b47576a8d9c9d1f520dfe9c86b76592d5a2db04
Kiesia Carmine <kiesia@be.autiful.net>
2013-06-06 15:02:48 +1200

    added basic simplecov support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,17 @@
+require 'simplecov'
+SimpleCov.start 'rails'
+
 require 'rubygems'
 require 'spork'
 require 'email_spec'
 require 'capybara/rspec'
+
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
-
 prefork = lambda {
   ENV["RAILS_ENV"] = 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-  # require 'rspec/autorun'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
