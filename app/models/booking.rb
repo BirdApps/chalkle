6486274c691662bee1805bd878d2b46d40031770
Kiesia Carmine <kiesia@be.autiful.net>
2013-06-11 14:00:18 +1200

    removed overzealous validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -11,7 +11,7 @@ class Booking < ActiveRecord::Base
   has_one :payment
 
   validates_uniqueness_of :chalkler_id, scope: :lesson_id
-  validates_presence_of :lesson_id, :chalkler_id, :payment_method, :status, :visible, :paid
+  validates_presence_of :lesson_id, :chalkler_id, :payment_method, :status
   validates_acceptance_of :terms_and_conditions, :on => :create, :message => 'please read and agree', :if => :enforce_terms_and_conditions
 
   scope :paid, where(paid: true)
