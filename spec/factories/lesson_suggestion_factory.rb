b8bef67d6d69731553e7db21b177db40ed72da17
Kiesia Carmine <kiesia@be.autiful.net>
2013-06-17 17:33:20 +1200

    refactored spec

--- spec/factories/lesson_suggestion_factory.rb
+++ spec/factories/lesson_suggestion_factory.rb
@@ -2,6 +2,9 @@ FactoryGirl.define do
   factory :lesson_suggestion do
     name "Suggesting fun"
     description "You should really learn, suggest it, it's fun!"
-    join_channels [1]
+
+    after(:build) do |lesson_suggestion|
+      lesson_suggestion.join_channels = [FactoryGirl.create(:channel).id]
+    end
   end
 end
