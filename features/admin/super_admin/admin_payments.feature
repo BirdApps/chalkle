d913f30cca503eb06b93bded03279ebdc401db8a
amypeng <amywpeng@gmail.com>
2013-06-19 22:18:17 +1200

    payment cucumber test

--- features/admin/super_admin/admin_payments.feature
+++ features/admin/super_admin/admin_payments.feature
@@ -19,6 +19,13 @@ Scenario: A payment with no details should be editable
   And they visit the "Edit" page
   Then they should see "Edit Payment"
 
+Scenario: Comments on payments should be shown on index page
+  Given there is an unreconciled payment with no details
+  And they click into this payment
+  And they fill in the comments with "This is a comment"
+  When they return to the payment index page
+  Then they should see "This is a comment"
+
 Scenario: Super admins can delete an unreconciled payment from any channel
   Given there is an unreconciled payment with no details
   When they visit the "Payments" tab
@@ -31,3 +38,11 @@ Scenario: Super admins can not delete a reconciled payment from any channel
   When they visit the "Payments" tab
   And they visit the "View" page
   Then they should not see the "Delete Payment" button
+
+Scenario: Super admins can reconcile payments to bookings
+  Given there is an ureconciled payment
+  When they visit the "Payments" tab
+  And they click to reconcile payments
+  Then they should see this payment
+  And they select the matching booking from the drop down
+  Then this payment should be reconciled
--- features/admin/super_admin/step_definitions/admin_payments_steps.rb
+++ features/admin/super_admin/step_definitions/admin_payments_steps.rb
@@ -10,8 +10,47 @@ Then /^they should see this payment$/ do
   page.should have_content("10")
 end
 
+And(/^they click into this payment$/) do 
+  click_link "Payments"
+  click_link "View"
+end
+
+And(/^they fill in the comments with "(.*?)"$/) do |comments|
+  fill_in 'active_admin_comment_body', with: comments
+  click_button 'Add Comment'
+end
+
+When(/^they return to the payment index page$/) do
+  visit admin_payments_path
+end
+
 Then /^this payment should be deleted$/ do
   payment = Payment.find_by_xero_id("abc")
   page.should have_content("Payment #{payment.id} deleted!")
   payment.visible.should be_false
+end
+
+Given(/^there is an ureconciled payment$/) do
+  FactoryGirl.create(:payment, xero_id: "abc", total: 10)
+  chalkler = FactoryGirl.create(:chalkler, name: "Test chalkler")
+  lesson = FactoryGirl.create(:lesson, name: "Test class", cost: 10, start_at: 1.day.from_now)
+  FactoryGirl.create(:booking, chalkler_id: chalkler.id, lesson_id: lesson.id, status: "yes", guests: 0, paid: false)
+end
+
+When(/^they click to reconcile payments$/) do
+  visit reconcile_admin_payments_path
+end
+
+Then(/^they select the matching booking from the drop down$/) do
+  payment = Payment.find_by_xero_id "abc"
+  option_xpath = "//*[@id='payment-#{payment.id}']/option[2]"
+  second_option = find(:xpath, option_xpath).text
+  chalkler = Chalkler.find_by_name "Test chalkler"
+  b = Booking.find_by_chalkler_id chalkler.id  
+  select("#{b.chalkler.name} - #{b.lesson.name}(#{b.lesson.meetup_id}) : #{b.lesson.start_at} : #{b.cost}", :from => "payment-#{payment.id}")
+end
+
+Then(/^this payment should be reconciled$/) do
+  payment = Payment.find_by_xero_id "abc"
+  payment.reconciled.should be_true
 end
