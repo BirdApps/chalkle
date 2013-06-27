65bab33d9b1ca8a973c82b9189b33efa6e1b4da8
amypeng <amywpeng@gmail.com>
2013-06-27 22:56:01 +1200

    add tests

--- app/models/booking_reminder.rb
+++ app/models/booking_reminder.rb
@@ -14,15 +14,17 @@ class BookingReminder
     chalklers.delete_if { |c| (c.email == nil || c.email == "") }
   end
 
+  private
+
   def remindable
     bookings = @chalkler.bookings.visible.confirmed.billable.unpaid.upcoming
-    bookings.delete_if { |b| (b.teacher? == true || b.lesson.channels.any? == false || b.lesson_start_at.present? == false) }
+    bookings.delete_if { |b| (b.teacher? == true || b.lesson.channels.any? == false || b.lesson_start_at.present? == false || b.cost == 0) }
   end
 
   def remind_now
     if remindable.any?
       bookings = remindable.keep_if {|b| (b.lesson_start_at <= (Time.now.utc + @lesson_start_in)) & (b.lesson_start_at > (Time.now.utc + @lesson_start_in - 1.day)) }
-      return bookings.sort_by{|b| b[:lesson_start_at]}
+      return bookings.sort_by{|b| b[:lesson_start_at]}.reverse
     else
       return []
     end
--- spec/models/booking_reminder_spec.rb
+++ spec/models/booking_reminder_spec.rb
@@ -2,241 +2,131 @@ require 'spec_helper'
 
 describe BookingReminder do
   describe "#initialize" do
-    let(:chalkler){ FactoryGirl.create(:chalkler, email_frequency: 'daily') }
-    let(:digest) { ChalklerDigest.new(chalkler) }
+    let(:chalkler){ FactoryGirl.create(:chalkler) }
+    let(:reminder) { BookingReminder.new(chalkler, 3.days) }
 
     it "sets @chalkler" do
-      digest.instance_eval{ @chalkler }.should == chalkler
+      reminder.instance_eval{ @chalkler }.should == chalkler
     end
 
-    it "sets @frequency" do
-      digest.instance_eval{ @frequency }.should == 'daily'
+    it "sets starting time of lessons to be reminded" do
+      reminder.instance_eval{ @lesson_start_in }.should == 3.days
     end
 
-    it "sets @limit" do
-      digest.instance_eval{ @limit }.should == 5
-    end
   end
 
-  describe "lesson selection" do
-    # this lesson should always be excluded from a set
-    let(:lesson1) {
-      FactoryGirl.create(:lesson,
-                         published_at: 1.day.ago,
-                         start_at: 1.day.from_now,
-                         status: 'Published',
-                         do_during_class: 'x',
-                         meetup_url: 'http://meetup.com',
-                         max_attendee: 10)
-    }
-
+  describe "booking selection" do
     before do
-      @category = FactoryGirl.create(:category)
-      @chalkler = FactoryGirl.create(:chalkler, email_categories: [@category.id], email_frequency: 'daily')
-      @channel = FactoryGirl.create(:channel, visible: true)
-      @chalkler.channels << @channel
-      @digest = ChalklerDigest.new(@chalkler)
-      @lesson = FactoryGirl.create(:lesson,
-                                   published_at: 1.day.ago,
-                                   start_at: 3.days.from_now,
-                                   status: 'Published',
-                                   do_during_class: 'x',
-                                   meetup_url: 'http://meetup.com',
-                                   max_attendee: 15)
-      @lesson.categories << @category
+      teacher = FactoryGirl.create(:chalkler, name: "Britany Spears", email: "britany@spears.com")
+      @chalkler = FactoryGirl.create(:chalkler, name: "Michael Jackson", email: "michael@jackson.com")
+      @channel = FactoryGirl.create(:channel, name: "Music")
+      @lesson = FactoryGirl.create(:lesson, name: "Chalkle Class 5", start_at: 3.days.from_now, cost: 10, teacher_id: teacher.id, visible: true)
+      @lesson.channels << @channel
+      @booking = FactoryGirl.create(:booking, chalkler_id: @chalkler.id, lesson_id: @lesson.id, status: 'yes', visible: true, paid: false)
+      @reminder = BookingReminder.new(@chalkler,3.days)
+    end
+
+    it "loads booking for reminder" do
+      @reminder.instance_eval{ remind_now }.should == [@booking]
+    end
+
+    it "won't load hidden bookings" do
+      @booking.update_attributes({:visible => false}, :as => :admin)
+      @reminder.instance_eval{ remindable }.should be_empty
+      @booking.update_attributes({:visible => true}, :as => :admin)
+    end
+
+    it "won't load bookings without a yes status" do
+      @booking.update_attributes({:status => 'waitlist'}, :as => :admin)
+      @reminder.instance_eval{ remindable }.should be_empty
+      @booking.update_attributes({:status => 'yes'}, :as => :admin)
+    end
+
+    it "won't load booking for a free class" do
+      @lesson.update_attributes({:cost => 0}, :as => :admin)
+      @reminder.instance_eval{ remindable }.should be_empty
+      @lesson.update_attributes({:cost => 10}, :as => :admin)
+    end
+
+    it "won't load free booking" do
+      @booking.update_attributes({:cost_override => 0}, :as => :admin)
+      @reminder.instance_eval{ remindable }.should be_empty
+      @booking.update_attributes({:cost_override => 0}, :as => :admin)
+    end
+
+    it "won't load a paid booking" do
+      @booking.update_attributes({:paid => true}, :as => :admin)
+      @reminder.instance_eval{ remindable }.should be_empty
+      @booking.update_attributes({:paid => false}, :as => :admin)
+    end
+
+    it "won't load booking from lessons in the past" do
+      @lesson.update_attributes({:start_at => 2.days.ago}, :as => :admin)
+      @reminder.instance_eval{ remindable }.should be_empty
+      @lesson.update_attributes({:start_at => 3.days.from_now}, :as => :admin)
+    end
+
+    it "won't load booking by the teacher" do
+      @booking.update_attributes({:chalkler_id => @lesson.teacher_id}, :as => :admin)
+      @reminder.instance_eval{ remindable }.should be_empty
+      @booking.update_attributes({:chalkler_id => @chalkler.id}, :as => :admin)
+    end
+
+    it "won't load booking for a lesson without a channel" do
+      @lesson.channels = []
+      @reminder.instance_eval{ remindable }.should be_empty
       @lesson.channels << @channel
     end
 
-    it "only loads new/open lessons once" do
-      channel = FactoryGirl.create(:channel)
-      @lesson.channels << channel
-      @chalkler.channels << channel
-      lessons = @digest.instance_eval{ new_lessons }
-      lessons.concat @digest.instance_eval{ open_lessons }
-      lessons.should == [@lesson]
+    it "won't load booking for a lesson without a start date" do
+      @lesson.update_attributes({:start_at => nil}, :as => :admin)
+      @reminder.instance_eval{ remindable }.should be_empty
+      @lesson.update_attributes({:start_at => 3.days.from_now}, :as => :admin)
     end
 
-    it "only loads default new/open lessons once" do
-      channel = FactoryGirl.create(:channel)
-      @lesson.channels << channel
-      @chalkler.channels << channel
-      lessons = @digest.instance_eval{ default_new_lessons }
-      lessons.concat @digest.instance_eval{ default_open_lessons }
-      lessons.should == [@lesson]
+    it "won't load booking from lessons more than 3 days away" do
+      @lesson.update_attributes({:start_at => 4.days.from_now}, :as => :admin)
+      @reminder.instance_eval{ remind_now }.should be_empty
+      @lesson.update_attributes({:start_at => 3.days.from_now}, :as => :admin)
     end
 
-    describe "#new_lessons" do
-      it "loads a lesson that a chalkler is interested in" do
-        lesson1.categories << FactoryGirl.create(:category)
-        lesson1.channels << @channel
-        @digest.instance_eval{ new_lessons }.should == [@lesson]
-      end
-
-      it "loads a lessons from channels that chalkler belongs to" do
-        lesson1.categories << @category
-        lesson1.channels << FactoryGirl.create(:channel)
-        @digest.instance_eval{ new_lessons }.should == [@lesson]
-      end
-
-      it "won't load a lesson without meetup_url" do
-        @lesson.update_attribute :meetup_url, nil
-        @digest.instance_eval{ new_lessons }.should be_empty
-      end
-
-      it "won't load a lesson without do_during_class" do
-        @lesson.update_attribute :do_during_class, nil
-        @digest.instance_eval{ new_lessons }.should be_empty
-      end
-
-      it "won't load a lesson that is more than 1 day old" do
-        @lesson.update_attribute :published_at, 2.days.ago
-        @digest.instance_eval{ new_lessons }.should be_empty
-      end
-
-      it "won't load a lesson from a hidden channel" do
-        @channel.update_attribute :visible, false
-        @digest.instance_eval{ new_lessons }.should be_empty
-      end
+    it "won't load booking from lessons less than 3 days away" do
+      @lesson.update_attributes({:start_at => 2.days.from_now}, :as => :admin)
+      @reminder.instance_eval{ remind_now }.should be_empty
+      @lesson.update_attributes({:start_at => 3.days.from_now}, :as => :admin)
     end
 
-    describe "#default_new_lessons" do
-      it "loads a lessons from channels that chalkler belongs to" do
-        lesson1.channels << FactoryGirl.create(:channel)
-        @digest.instance_eval{ default_new_lessons }.should == [@lesson]
-      end
-
-      it "won't load a lesson without meetup_url" do
-        @lesson.update_attribute :meetup_url, nil
-        @digest.instance_eval{ default_new_lessons }.should be_empty
-      end
-
-      it "won't load a lesson without do_during_class" do
-        @lesson.update_attribute :do_during_class, nil
-        @digest.instance_eval{ default_new_lessons }.should be_empty
-      end
-
-      it "won't load a lesson that is more than 1 day old" do
-        @lesson.update_attribute :published_at, 2.days.ago
-        @digest.instance_eval{ default_new_lessons }.should be_empty
-      end
-
-      it "won't load a lesson from a hidden channel" do
-        @channel.update_attribute :visible, false
-        @digest.instance_eval{ default_new_lessons }.should be_empty
-      end
+    it "will sort lessons in order of start time" do
+      lesson = FactoryGirl.create(:lesson, name: "An earlier lesson", cost: 5, teacher_id: @lesson.teacher_id, visible: true, start_at: @lesson.start_at - 5.hours)
+      lesson.channels << @channel
+      booking = FactoryGirl.create(:booking, chalkler_id: @chalkler.id, lesson_id: lesson.id, status: 'yes', paid: false, visible: true)
+      @reminder.instance_eval{ remind_now }.should == [booking, @booking]
     end
-
-    describe "#open_lessons" do
-      before do
-        @lesson.update_attribute :published_at, 3.days.ago
-        lesson1.update_attribute :published_at, 3.days.ago
-      end
-
-      it "loads a lesson that a chalkler is interested in" do
-        lesson1.categories << FactoryGirl.create(:category)
-        lesson1.channels << @channel
-        @digest.instance_eval{ open_lessons }.should == [@lesson]
-      end
-
-      it "loads a lessons from channels that chalkler belongs to" do
-        lesson1.categories << @category
-        lesson1.channels << FactoryGirl.create(:channel)
-        @digest.instance_eval{ open_lessons }.should == [@lesson]
-      end
-
-      it "won't load a full lesson" do
-        @lesson.update_attribute :max_attendee, 10
-        @lesson.bookings = []
-        10.times { FactoryGirl.create(:booking, lesson: @lesson) }
-        @digest.instance_eval{ open_lessons }.should be_empty
-      end
-
-      it "won't load a lesson without meetup_url" do
-        @lesson.update_attribute :meetup_url, nil
-        @digest.instance_eval{ open_lessons }.should be_empty
-      end
-
-      it "won't load a lesson without do_during_class" do
-        @lesson.update_attribute :do_during_class, nil
-        @digest.instance_eval{ open_lessons }.should be_empty
-      end
-
-      it "won't load a lesson that begins less than one day from now" do
-        @lesson.update_attribute :start_at, Time.now.utc + 23.hours
-        @digest.instance_eval{ open_lessons }.should be_empty
-      end
-
-      it "won't choke on an empty set" do
-        @lesson.destroy
-        @digest.instance_eval{ open_lessons }.should be_empty
-      end
-
-      it "won't load a lesson from a hidden channel" do
-        @channel.update_attribute :visible, false
-        @digest.instance_eval{ open_lessons }.should be_empty
-      end
-    end
-
-    describe "#default_open_lessons" do
-      before do
-        @lesson.update_attribute :published_at, 3.days.ago
-        lesson1.update_attribute :published_at, 3.days.ago
-      end
-
-      it "loads a lesson from channels that chalkler belongs to" do
-        lesson1.categories << @category
-        lesson1.channels << FactoryGirl.create(:channel)
-        @digest.instance_eval{ default_open_lessons }.should == [@lesson]
-      end
-
-      it "won't load a full lesson" do
-        @lesson.update_attribute :max_attendee, 10
-        @lesson.bookings = []
-        10.times { FactoryGirl.create(:booking, lesson: @lesson) }
-        @digest.instance_eval{ open_lessons }.should be_empty
-      end
-
-      it "won't load a lesson without meetup_url" do
-        @lesson.update_attribute :meetup_url, nil
-        @digest.instance_eval{ default_open_lessons }.should be_empty
-      end
-
-      it "won't load a lesson without do_during_class" do
-        @lesson.update_attribute :do_during_class, nil
-        @digest.instance_eval{ default_open_lessons }.should be_empty
-      end
-
-      it "won't load a lesson that begins less than one day from now" do
-        @lesson.update_attribute :start_at, Time.now.utc + 23.hours
-        @digest.instance_eval{ open_lessons }.should be_empty
-      end
-
-      it "won't choke on an empty set" do
-        @lesson.destroy
-        @digest.instance_eval{ open_lessons }.should be_empty
-      end
-
-      it "won't load a lesson from a hidden channel" do
-        @channel.update_attribute :visible, false
-        @digest.instance_eval{ open_lessons }.should be_empty
-      end
-    end
+  
   end
-
+    
   describe ".load_chalklers" do
+    before do
+      @chalkler = FactoryGirl.create(:chalkler, name: "Britany Spears", email: "britany@spears.com")
+      @chalkler1 = FactoryGirl.create(:chalkler, email: nil, meetup_id: 12345678)
+      @lesson = FactoryGirl.create(:lesson, name: "Chalkle Class 5", start_at: 3.days.from_now, cost: 10, visible: true)
+      FactoryGirl.create(:booking, chalkler_id: @chalkler.id, lesson_id: @lesson.id, status: 'yes', visible: true, paid: false)
+      FactoryGirl.create(:booking, chalkler_id: @chalkler1.id, lesson_id: @lesson.id, status: 'yes', visible: true, paid: false)
+    end
+
     it "won't return a chalkler without an email address" do
-      FactoryGirl.create(:chalkler, email: nil, email_frequency: 'weekly', meetup_id: 12345678)
-      ChalklerDigest.load_chalklers('weekly').should be_empty
+      BookingReminder.load_chalklers.should_not include(@chalkler1)
     end
 
-    it "won't returns a chalkler without correct frequency" do
-      c = FactoryGirl.create(:chalkler, email: 'example@example.com', email_frequency: 'weekly')
-      ChalklerDigest.load_chalklers('daily').should be_empty
+    it "return a chalkler with an email address" do
+      BookingReminder.load_chalklers.should include(@chalkler)
     end
 
-    it "will return a chalkler when email and correct email_frequency set" do
-      c = FactoryGirl.create(:chalkler, email: 'example@example.com', email_frequency: 'weekly')
-      ChalklerDigest.load_chalklers('weekly').should == [c]
+    it "won't return duplicated chalklers" do
+      lesson2 = FactoryGirl.create(:lesson, name: "Another lesson", start_at: 3.days.from_now, cost: 10, visible: true)
+      FactoryGirl.create(:booking, chalkler_id: @chalkler.id, lesson_id: lesson2.id, status: 'yes', visible: true, paid: false)
+      BookingReminder.load_chalklers.should == [@chalkler]
     end
   end
+
 end
