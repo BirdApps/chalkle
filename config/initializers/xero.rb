09a8682f5d97da51e29b5dc92e1f2680fedcf5d3
Craig Ambrose <craig@craigambrose.com>
2013-10-14 20:33:52 +1300

    updated xero config

--- config/initializers/xero.rb
+++ config/initializers/xero.rb
@@ -1,2 +1,2 @@
-Payment.xero_consumer_key = 'V7LDE2QHGE0OTLWYA8QKBLDNDSUDG3'
-Payment.xero_consumer_secret = 'JF7EUJRNHDB90B9LVZODCLRHPEZ0QA'
+Payment.xero_consumer_key = 'EKMAYKP6LGJJPPBTGEI7WENGNCENT9'
+Payment.xero_consumer_secret = 'MILTJLIYEFN386DBPARTVH6XAFHXZD'
