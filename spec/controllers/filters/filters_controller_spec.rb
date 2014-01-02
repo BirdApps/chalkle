5be9e3aa670eea6732b51e2f98accffd427ec1b9
Craig Ambrose <craig@craigambrose.com>
2014-01-03 11:56:03 +1300

    Non logged in user filters work

--- spec/controllers/filters/filters_controller_spec.rb
+++ spec/controllers/filters/filters_controller_spec.rb
@@ -9,8 +9,8 @@ module Filters
         @region = FactoryGirl.create :region
       end
 
-      def current_rule_should_be(key, value)
-        filter = @current_chalkler.lesson_filter
+      def current_rule_should_be(key, value, filter = nil)
+        filter ||= @current_chalkler.lesson_filter
         filter.should_not be_nil
         filter.rules.length.should == 1
         filter.rules.first.strategy_name.should == key
@@ -43,6 +43,33 @@ module Filters
             put :update, id: 'single_region', value: @region.id.to_s
 
             current_rule_should_be 'single_region', @region.id.to_s
+          end
+        end
+      end
+
+      context "with a guest user" do
+        context "with no filter" do
+          it "creates a filter and stores it in the session" do
+            put :update, id: 'single_region', value: @region.id.to_s
+
+            filter = Filter.last
+            filter.should_not be_nil
+            current_rule_should_be 'single_region', @region.id.to_s, filter
+
+            session[:filter_id].should == filter.id
+          end
+        end
+
+        context "with a filter in the session" do
+          it "sets the rule on the existing filter" do
+            filter = Filter.create!
+            session[:filter_id] = filter.id
+
+            put :update, id: 'single_region', value: @region.id.to_s
+
+            Filter.count.should == 1
+            current_rule_should_be 'single_region', @region.id.to_s, filter
+            session[:filter_id].should == filter.id
           end
         end
       end
