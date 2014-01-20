68459c0fea864df753129ef11823f1f931c034d5
Craig Ambrose <craig@craigambrose.com>
2014-01-20 16:53:50 +1300

    Removed meetup email instructions

--- app/controllers/chalklers/preferences_controller.rb
+++ app/controllers/chalklers/preferences_controller.rb
@@ -2,11 +2,11 @@ class Chalklers::PreferencesController < Chalklers::BaseController
   def save
    	@chalkler_email_preferences = ChalklerPreferences.new(current_chalkler)
 
-      if @chalkler_email_preferences.update_attributes(params[:chalkler_preferences])
-        redirect_to chalklers_meetup_email_settings_url, notice: 'Your preferences have been saved.'
-      else
-        render template: 'chalklers/dashboard/index'
-      end
+    if @chalkler_email_preferences.update_attributes(params[:chalkler_preferences])
+      redirect_to root_path, notice: 'Your preferences have been saved.'
+    else
+      render template: 'chalklers/dashboard/index'
+    end
   end
 
   def show
