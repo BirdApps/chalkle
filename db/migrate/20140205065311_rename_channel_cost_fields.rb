113cb28596be57957bd12882328caab70a64d8e5
Craig Ambrose <craig@craigambrose.com>
2014-02-05 21:01:54 +1300

    fixed the migration

--- db/migrate/20140205065311_rename_channel_cost_fields.rb
+++ db/migrate/20140205065311_rename_channel_cost_fields.rb
@@ -2,7 +2,7 @@ class RenameChannelCostFields < ActiveRecord::Migration
   def up
     rename_column :channels, :channel_percentage, :channel_rate_override
     change_column :channels, :channel_rate_override, :decimal, :precision => 8, :scale => 4, :default => nil
-    execute "UPDATE channels SET channel_rate_override = NULL WHERE cost_model_id = NULL"
+    execute "UPDATE channels SET channel_rate_override = NULL WHERE cost_model_id IS NULL"
   end
 
   def down
