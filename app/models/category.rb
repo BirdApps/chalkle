374c0d7d4814309549e3aae5693fab5447743e34
Samson Ootoovak <sam@enspiral.com>
2014-03-10 12:17:14 +1300

    Fix category creation by admins by adding 'primary' attribute to accessible list.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   attr_accessible :name, :colour_num, :parent_id, :parent, as: :default
-  attr_accessible :name, :colour_num, :parent_id, :parent, as: :admin
+  attr_accessible :name, :colour_num, :parent_id, :parent, :primary, as: :admin
 
   has_many :channel_categories
   has_many :channels, :through => :channel_categories
