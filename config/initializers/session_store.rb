c1e49315238717139f1151d52c945d82362a698c
Craig Ambrose <craig@craigambrose.com>
2014-04-10 19:55:20 +1200

    Share sessions across domains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-Chalkle::Application.config.session_store :cookie_store, key: '_chalkle_session'
+Chalkle::Application.config.session_store(
+  :cookie_store,
+  key: '_chalkle_session',
+  domain: :all,
+  tld_length: ActionDispatch::Http::URL.tld_length + 1
+)
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
