5a513434838ab75bcfd8e807f99205559c690cc2
matthew.kerr@me.com <matthew.kerr@me.com>
2014-04-30 13:03:14 +1200

    MIGRATION: Chalklers following the wellington channel are associated with the wellington region.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140414030150) do
+ActiveRecord::Schema.define(:version => 20140429235339) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
