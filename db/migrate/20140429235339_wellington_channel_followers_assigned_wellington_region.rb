c13dd10971cb8d5a04473f817b47085a885c4f24
matthew.kerr@me.com <matthew.kerr@me.com>
2014-05-06 11:52:01 +1200

    JNNuG07O migration updated for chalklers without any current email regions

--- db/migrate/20140429235339_wellington_channel_followers_assigned_wellington_region.rb
+++ db/migrate/20140429235339_wellington_channel_followers_assigned_wellington_region.rb
@@ -4,7 +4,11 @@ class WellingtonChannelFollowersAssignedWellingtonRegion < ActiveRecord::Migrati
     wellington_chalklers = Channel.where(name: "Wellington").map(&:chalklers).flatten!
     transaction do 
       wellington_chalklers.each {|wc| 
-        wc.email_region_ids << wellington_region_id 
+        if wc.email_region_ids 
+          wc.email_region_ids << wellington_region_id 
+        else 
+            wc.email_region_ids = [ wellington_region_id ]
+        end
         if wc.save
           puts "Chalkler \"#{wc.name}\" added to wellington region" 
         end
