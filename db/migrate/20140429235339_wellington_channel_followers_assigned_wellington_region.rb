9d4928d8c0ef5026e240912fc6bc4005b7bc8b76
Matthew Kerr <matthew.kerr@me.com>
2014-05-07 15:13:46 +1200

    update migration

--- db/migrate/20140429235339_wellington_channel_followers_assigned_wellington_region.rb
+++ db/migrate/20140429235339_wellington_channel_followers_assigned_wellington_region.rb
@@ -1,18 +1,19 @@
 class WellingtonChannelFollowersAssignedWellingtonRegion < ActiveRecord::Migration
   def up
-    wellington_region_id = Region.find_by_name("Wellington").id
-    wellington_chalklers = Channel.where(name: "Wellington").map(&:chalklers).flatten!
-    transaction do 
-      wellington_chalklers.each {|wc| 
-        if wc.email_region_ids 
-          wc.email_region_ids << wellington_region_id 
-        else 
-            wc.email_region_ids = [ wellington_region_id ]
-        end
-        if wc.save
-          puts "Chalkler \"#{wc.name}\" added to wellington region" 
-        end
-      }
+    if wellington_region_id = Region.find_by_name("Wellington").try(:id)
+      wellington_chalklers = Channel.where(name: "Wellington").map(&:chalklers).flatten!
+      transaction do 
+        wellington_chalklers.each {|wc| 
+          if wc.email_region_ids 
+            wc.email_region_ids << wellington_region_id 
+          else 
+              wc.email_region_ids = [ wellington_region_id ]
+          end
+          if wc.save
+            puts "Chalkler \"#{wc.name}\" added to wellington region" 
+          end
+        }
+      end
     end
   end
 
