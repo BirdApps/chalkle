da88bbeaee5943dc53956c3d01fdbfd76c21a39a
matthew.kerr@me.com <matthew.kerr@me.com>
2014-06-26 11:07:38 +1200

    migration to remove wellington region from all chalklers

---
+++ db/migrate/20140625222351_remove_wellington_chalklers_from_wellington_region.rb
@@ -0,0 +1,22 @@
+class RemoveWellingtonChalklersFromWellingtonRegion < ActiveRecord::Migration
+  def up
+    wellington_region_id = Region.where(name: 'Wellington').map(&:id).first
+    wellington_chalklers = Chalkler.all.select {|c| c.email_region_ids.include? wellington_region_id }
+
+    transaction do 
+      wellington_chalklers.each do |c| 
+        c.email_region_ids.delete(wellington_region_id)
+        if c.save
+          puts "#{c.name} removed from wellington region"
+        else
+          puts "#{c.name} removed from wellington region FAILED"
+          puts c.errors
+        end
+      end
+    end
+  end
+
+  def down
+    ActiveRecord::IrreversibleMigration
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140521000556) do
+ActiveRecord::Schema.define(:version => 20140625222351) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -148,9 +148,9 @@ ActiveRecord::Schema.define(:version => 20140521000556) do
     t.datetime "created_at",                                                             :null => false
     t.datetime "updated_at",                                                             :null => false
     t.string   "url_name"
+    t.string   "email"
     t.decimal  "channel_rate_override", :precision => 8, :scale => 4
     t.decimal  "teacher_percentage",    :precision => 8, :scale => 4, :default => 0.75
-    t.string   "email"
     t.string   "account"
     t.boolean  "visible",                                             :default => false
     t.text     "description"
