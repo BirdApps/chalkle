f10a585ba4b9fb615379066b93b640b3be26b294
matthew.kerr@me.com <matthew.kerr@me.com>
2014-06-26 14:14:07 +1200

    migration fixed

--- db/migrate/20140625222351_remove_wellington_chalklers_from_wellington_region.rb
+++ db/migrate/20140625222351_remove_wellington_chalklers_from_wellington_region.rb
@@ -1,15 +1,18 @@
 class RemoveWellingtonChalklersFromWellingtonRegion < ActiveRecord::Migration
   def up
     wellington_region_id = Region.where(name: 'Wellington').map(&:id).first
-    wellington_chalklers = Chalkler.with_email_region_id wellington_region_id
+    wellington_channel_chalklers = Chalkler.all.select do |c| 
+      c.channels.include? Channel.where(name: "Wellington").first.id 
+    end
 
     transaction do 
-      wellington_chalklers.each do |c| 
-        c.email_region_ids.delete(wellington_region_id)
+      wellington_channel_chalklers.each do |c| 
+        c.email_region_ids + wellington_region_id 
+        c.channels.delete Channel.where(name: "Wellington").first
         if c.save
-          puts "#{c.name} removed from wellington region"
+          puts "#{c.name} removed from wellington channel"
         else
-          puts "#{c.name} removed from wellington region FAILED"
+          puts "#{c.name} removed from wellington channel FAILED"
           puts c.errors
           raise ActiveRecord::Rollback
         end
