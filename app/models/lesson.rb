3e416f872625e32568ebed3ab86b4c8e48eb42bd
Josh Dean <jdbdean@gmail.com>
2014-07-10 16:57:48 +1200

    Revert "Added cancelled to lesson"
    This reverts commit 2362e559d1afd7bf3e35b7d7fedac1c911d6f7dd.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,19 +1,13 @@
 class Lesson < ActiveRecord::Base
 	
   attr_accessible *BASIC_ATTR = [
-    :start_at, :duration, :course_id, :cancelled
+    :start_at, :duration, :course_id
   ]
 
   attr_accessible *BASIC_ATTR, :as => :admin
 
   belongs_to :course
 
-  validates_format_of :duration, with: /^[+]?([1-9][0-9]*(?:[\.][0-9]*)?|0*\.0*[1-9][0-9]*)(?:[eE][+-][0-9]+)?$/
-
-  validate :starts_within_a_century
+  validates_presence_of :course
   
-  def starts_within_a_century
-    errors.add(:start_at, "is not within a century") unless start_at <= 100.years.from_now && start_at >= 100.years.ago
-  end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140709233752) do
+ActiveRecord::Schema.define(:version => 20140708040822) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -271,7 +271,6 @@ ActiveRecord::Schema.define(:version => 20140709233752) do
     t.integer  "course_id"
     t.datetime "start_at"
     t.decimal  "duration"
-    t.boolean  "cancelled", :default => false
   end
 
   create_table "omni_avatar_avatars", :force => true do |t|
