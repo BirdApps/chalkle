cc151e30fdf32ba356fe913ac6e9d5a3bf6824f8
Josh Dean <jdbdean@gmail.com>
2014-08-01 11:27:08 +1200

    Added url_name to category

--- db/migrate/20140731032429_add_column_to_course.rb
+++ db/migrate/20140731032429_add_column_to_course.rb
@@ -3,7 +3,7 @@ class AddColumnToCourse < ActiveRecord::Migration
     add_column :courses, :url_name, :string
     Course.reset_column_information
     Course.all.each do |course|
-      course.update_attribute(:url_name, course.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '') )
+      course.update_attribute(:url_name, course.name.parameterize )
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140731032429) do
+ActiveRecord::Schema.define(:version => 20140731224601) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(:version => 20140731032429) do
     t.integer  "parent_id"
     t.integer  "colour_num"
     t.boolean  "primary",    :default => false
+    t.string   "url_name"
   end
 
   create_table "chalklers", :force => true do |t|
