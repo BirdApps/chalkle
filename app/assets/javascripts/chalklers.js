2e6b75d85e29fa4cf32c63b02285b52c4169d5bd
Josh Dean <jdbdean@gmail.com>
2014-08-05 12:56:32 +1200

    javascript new course bad input fix

--- app/assets/javascripts/chalklers.js
+++ app/assets/javascripts/chalklers.js
@@ -18,13 +18,14 @@ $(function() {
     }
     if(hours > 12) {
       hours = hours % 12;
-      if(hours == 0){
+      if(hours == 0) {
         hours = 12
-      }
+      }      
     }
+
     if(minutes > 59){
       minutes = minutes % 60;
-      hours = hours + minutes/60;
+      hours = hours + Math.floor(minutes/60);
     }
     if(minutes < 10){
       minutes = "0"+minutes
@@ -75,38 +76,58 @@ $(function() {
   }
 
   function calculate_class_times() {
-    if(instance_date && instance_time && duration_hours && duration_minutes){
+    update_durations();
+    if(instance_date && instance_time && ( duration_hours || duration_minutes )){
       var start_time = make_time(instance_date.getHours(), instance_date.getMinutes(),0,0);
       var end_time = make_time(instance_date.getHours(), instance_date.getMinutes(), duration_hours, duration_minutes);
       var string_build = "Your class will be held ";
-      repeat_times = $("#teaching_repeat_times").val();
-      if(repeating && repeat_times) {
-          if(monthly){
-            var wday = instance_date.getDay();
-            var date_lapse = new Date();
-            date_lapse.setYear(instance_date.getFullYear());
-            date_lapse.setMonth(instance_date.getMonth() + parseInt(repeat_times) - 1);
-            date_lapse.setDate(1);
-            var target_nth = nth_day_instance_date();
-            var end_date = nth_day_of(target_nth, wday, date_lapse);
-            string_build += " on the "+ordinal(target_nth)+" "+weekdays[wday]+" of every month from "+instance_date.toDateString()+" until "+end_date.toDateString();
-          }else{
-            var end_date = new Date();
-            end_date.setMonth(instance_date.getMonth());
-            end_date.setDate(instance_date.getDate()+7*(repeat_times-1));
-            string_build += " weekly from "+instance_date.toDateString()+" until "+end_date.toDateString();
-          }
-        }else{
-          string_build += " on "+instance_date.toDateString(); 
-        }
-        string_build += " between "+start_time;
-        string_build += " and "+end_time;
-        $('#class-repeat-times').html(string_build);
-        $('#class-repeat-times').show();
-      } else {
-        $('#class-repeat-times').hide();
+      repeat_times = parseInt($("#teaching_repeat_times").val());
+      repeating = repeating && repeat_times > 1
+      if(repeating && monthly) {
+        //monthly
+        var wday = instance_date.getDay();
+        var date_lapse = new Date();
+        date_lapse.setYear(instance_date.getFullYear());
+        date_lapse.setMonth(instance_date.getMonth() + parseInt(repeat_times) - 1);
+        date_lapse.setDate(1);
+        var target_nth = nth_day_instance_date();
+        var end_date = nth_day_of(target_nth, wday, date_lapse);
+        string_build += " on the "+ordinal(target_nth)+" "+weekdays[wday]+" of every month from "+instance_date.toDateString()+" until "+end_date.toDateString();
+      }else if(repeating && !monthly){
+        //weekly
+        var end_date = new Date();
+        end_date.setMonth(instance_date.getMonth());
+        end_date.setDate(instance_date.getDate()+7*(repeat_times-1));
+        string_build += " weekly from "+instance_date.toDateString()+" until "+end_date.toDateString();
+      }else if(!repeating){
+        //once-off
+        string_build += " on "+instance_date.toDateString();  
       }
+         
+      class_length_in_hours = (instance_date.getHours()*60 + duration_hours*60+duration_minutes)/60;
+      if(class_length_in_hours > 24){
+        //more than a day long
+        start_time = weekdays[instance_date.getDay()] + " " + start_time
+        end_date = instance_date_clone();
+        end_date.setDate(end_date.getDate() + Math.floor(class_length_in_hours/24));
+        end_time = weekdays[end_date.getDay()] + " " + end_time;
+      }
+      string_build += " between "+start_time;
+      string_build += " and "+end_time;
+      $('#class-repeat-times').html(string_build);
+      $('#class-repeat-times').show();
+    } else {
+      $('#class-repeat-times').hide();
     }
+  }
+
+  function instance_date_clone(){
+    clone = new Date();
+    clone.setYear(instance_date.getFullYear());
+    clone.setMonth(instance_date.getMonth());
+    clone.setDate(instance_date.getDate());
+    return clone;
+  }
 
   function repeat_frequency_select() {
     monthly = $('#teaching_repeat_frequency').val() == 'monthly';
@@ -128,9 +149,14 @@ $(function() {
   }
 
   function update_durations(){
-    duration_hours = $('#teaching_duration_hours').val();
-    duration_minutes = $('#teaching_duration_minutes').val();
-    calculate_class_times();
+    duration_hours = Math.floor($('#teaching_duration_hours').val() ? parseInt($('#teaching_duration_hours').val())  : 0 );
+    duration_minutes = Math.floor($('#teaching_duration_minutes').val() ? parseInt($('#teaching_duration_minutes').val())  : 0 )
+    if( isNaN(duration_hours) ) { duration_hours = 0 } 
+    if( isNaN(duration_minutes) ) { duration_minutes = 0 }
+    if( duration_hours > 167 ) { duration_hours = 167 }
+    if( duration_minutes > 59 ) { duration_minutes = 59 }
+    $('#teaching_duration_minutes').val(duration_minutes);
+    $('#teaching_duration_hours').val(duration_hours) 
   }
 
   function repeating_course_init() {
@@ -140,8 +166,8 @@ $(function() {
     $('#teaching_repeat_frequency').change(repeat_frequency_select);
     $('#teaching_repeating').change(repeating_course_select);
     $('#teaching_repeat_times').change(calculate_class_times);
-    $('#teaching_duration_minutes').change(update_durations);
-    $('#teaching_duration_hours').change(update_durations);
+    $('#teaching_duration_minutes').change(calculate_class_times);
+    $('#teaching_duration_hours').change(calculate_class_times);
   }
 
   function update_teaching_start_at() {
