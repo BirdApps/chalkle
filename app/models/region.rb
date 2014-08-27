40aaec329b50c85e50befbf59cc837547b04cc15
Josh Dean <jdbdean@gmail.com>
2014-08-28 09:29:37 +1200

    Made url_name accessible

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,6 +1,6 @@
 # Geographical area below country
 class Region < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :url_name
   attr_accessible :name, :url_name, as: :admin
   default_scope order('name ASC')
 end
