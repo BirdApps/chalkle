57321ad21d5ff694b77e6d6079d26fe6063262cf
Josh Dean <jdbdean@gmail.com>
2014-09-01 15:57:46 +1200

    added basic search functionality

---
+++ app/controllers/v2/search_controller.rb
@@ -0,0 +1,16 @@
+class V2::SearchController < V2::BaseController
+  def index
+    classes
+  end
+
+  def classes
+    @courses = []
+    query = params[:q]
+    courses = Course.arel_table
+    if query
+      query_parts = query.split.map { |part| "%#{part}%" }
+      @courses = Course.where courses[:name].matches_any(query_parts)
+    end
+    render :classes
+  end
+end
