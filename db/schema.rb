3292a973c9839d8b26b2c9db1ff5d41590b8a950
Josh Dean <jdbdean@gmail.com>
2014-09-04 18:08:24 +1200

    added visible to chalkler

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140902001511) do
+ActiveRecord::Schema.define(:version => 20140904051119) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(:version => 20140902001511) do
     t.text     "email_categories"
     t.string   "phone_number"
     t.text     "email_region_ids"
+    t.boolean  "visible",                :default => true
   end
 
   create_table "channel_admins", :force => true do |t|
