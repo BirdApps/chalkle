1ad2422fcbb30427c176592ef3d3df4ac55d281f
Josh Dean <jdbdean@gmail.com>
2014-09-10 11:49:53 +1200

    Pundit policies for courses

---
+++ app/policies/course_policy.rb
@@ -0,0 +1,21 @@
+class CoursePolicy < ApplicationPolicy
+  attr_reader :user, :course
+
+  def initialize(user, course)
+    @user = user
+    @course = course
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    @user.chalkler.channel_teachers.include? @course.teacher or @user.chalkler.channel_admins.where(channel_id: @course.channel_id).present? or @user.admin_user.super?
+  end
+
+  def change_status?
+    update?
+  end
+
+end
