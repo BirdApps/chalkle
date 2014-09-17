42ad8bba7b6c9bb0d477d9cecae08609442fe825
Matthew Kerr <matthew.kerr@me.com>
2014-09-18 10:36:35 +1200

    Hey I changed the logo size

--- app/uploaders/channel_logo_uploader.rb
+++ app/uploaders/channel_logo_uploader.rb
@@ -3,6 +3,6 @@ require 'chalkle_base_image_uploader'
 
 class ChannelLogoUploader < ChalkleBaseImageUploader
 
-  process :resize_to_fill => [165, 165]
+  process :resize_to_fill => [1000, 1000]
 
 end
