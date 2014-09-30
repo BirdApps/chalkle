a793f32c4f1a66cf81f07c169ce154b138fcc134
Matthew Kerr <matthew.kerr@me.com>
2014-10-01 11:17:07 +1300

    update staging unicorn conf to handle forks

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -14,3 +14,28 @@ timeout 30
 
 
 preload_app true
+
+
+before_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection(
+      Rails.application.config.database_configuration[Rails.env]
+    )
+
+end
