10dd3d49526077a7a24e4d8412df8ba3e50fd08b
Matthew Kerr <matthew.kerr@me.com>
2014-10-04 19:14:30 +1300

    channel logos work.

--- app/uploaders/channel_logo_uploader.rb
+++ app/uploaders/channel_logo_uploader.rb
@@ -3,6 +3,6 @@ require 'chalkle_base_image_uploader'
 
 class ChannelLogoUploader < ChalkleBaseImageUploader
 
-  process :resize_to_fill => [1000, 1000]
+  process :resize_to_fill => [400, 400]
 
 end
