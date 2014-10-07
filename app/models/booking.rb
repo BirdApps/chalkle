373353a3948218095da45756d324f4a5fa7290a3
Josh Dean <jdbdean@gmail.com>
2014-10-08 12:36:44 +1300

    fixed fee saving and swype error

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,7 @@ class Booking < ActiveRecord::Base
   attr_accessible *BASIC_ATTR, :chalkler_id, :chalkler, :course, :status, :cost_override, :paid, :visible, :reminder_last_sent_at, :chalkle_fee, :chalkle_gst, :chalkle_gst_number, :teacher_fee, :teacher_gst, :teacher_gst_number, :provider_fee , :provider_gst, :provider_gst_number, :processing_fee, :processing_gst, :as => :admin
 
   #booking statuses
+  STATUS_5 = "pending" #payment pending
   STATUS_4 = "refund_complete"
   STATUS_3 = "refund_pending"
   STATUS_2 = "no"
@@ -37,17 +38,20 @@ class Booking < ActiveRecord::Base
   before_validation :set_free_course_attributes
 
   delegate :start_at, :venue, :prerequisites, :teacher_id, :cose, to: :course, prefix: true
-  delegate :free?, to: :course, allow_nil: true
+  #delegate :free?, to: :course, allow_nil: true
+  def free?
+    cost == 0
+  end
 
   def cancel!
     #TODO: notify teacher
-    @booking.status = 'no'
-    if @booking.refundable?
-      if @booking.paid && @booking.paid > 0
-        @booking.status = 'refund_pending'
+    self.status = 'no'
+    if refundable?
+      if paid && paid > 0
+        self.status = 'refund_pending'
       end
     end
-    @booking.save
+    save
   end
 
   def paid?
@@ -58,42 +62,36 @@ class Booking < ActiveRecord::Base
     chalkle_gst_number
   end
 
-  def waive_chalkle_fee
-    true
-  end
-
   def remove_fees
     chalkle_fee = chalkle_gst = teacher_fee = teacher_gst = provider_fee = provider_gst = 0
   end
 
   def apply_fees
-    chalkle_gst_number = processing_gst_number =  Finance::CHALKLE_GST_NUMBER
-    chalkle_fee = course.chalkle_fee false
-    chalkle_gst = course.chalkle_fee(true) - chalkle_fee
+    self.chalkle_gst_number =  Finance::CHALKLE_GST_NUMBER
+    self.chalkle_fee = course.chalkle_fee false
+    self.chalkle_gst = course.chalkle_fee(true) - chalkle_fee
     
-    processing_fee = course.processing_fee
-    processing_gst = course.processing_fee*3/23
+    self.processing_fee = course.processing_fee
+    self.processing_gst = course.processing_fee*3/23
 
     if course.teacher_pay_type == Course.teacher_pay_types[1]
-      teacher_fee = course.teacher_cost
+      self.teacher_fee = course.teacher_cost
       if course.teacher.tax_number
-        teacher_gst = teacher_fee*3/23
-        teacher_gst_number = course.teacher.tax_number
+        self.teacher_gst = teacher_fee*3/23
+        self.teacher_gst_number = course.teacher.tax_number
       end
     end
 
-    provider_fee = course.channel_fee
+    self.provider_fee = course.channel_fee
     if channel.tax_number
-      provider_gst_number = channel.tax_number
-      provider_gst = course.channel_fee*3/23
+      self.provider_gst_number = channel.tax_number
+      self.provider_gst = course.channel_fee*3/23
     end
+    cost
   end
 
   def cost
-    (chalkle_fee||0)    + (chalkle_gst||0)   +
-    (teacher_fee||0)    + (teacher_gst||0)   +
-    (provider_fee||0)   + (provider_gst||0)  +
-    (processing_fee||0) + (processing_gst||0)
+    (chalkle_fee||0)+(chalkle_gst||0)+(teacher_fee||0)+(teacher_gst||0)+(provider_fee||0)+(provider_gst||0)+(processing_fee||0)+(processing_gst||0)
   end
 
   def cost_formatted
