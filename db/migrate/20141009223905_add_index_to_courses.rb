599044975d4d0efd16665cf9748024a77b860a4c
Matthew Kerr <matthew.kerr@me.com>
2014-10-10 12:22:58 +1300

    fix indexfest migration

--- db/migrate/20141009223905_add_index_to_courses.rb
+++ db/migrate/20141009223905_add_index_to_courses.rb
@@ -4,6 +4,5 @@ class AddIndexToCourses < ActiveRecord::Migration
     add_index :courses, :status
     add_index :courses, :region_id
     add_index :courses, :url_name
-    add_index :courses, :url_name
   end
 end
