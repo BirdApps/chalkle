fd332cab084edee77b87f3c04b953bf9716814be
Matthew Kerr <matthew.kerr@me.com>
2014-10-10 16:54:25 +1300

    add data fix in migrations for prod

--- db/migrate/20141010001553_add_end_at_to_courses.rb
+++ db/migrate/20141010001553_add_end_at_to_courses.rb
@@ -1,5 +1,9 @@
 class AddEndAtToCourses < ActiveRecord::Migration
   def up
+    Lesson.transaction {
+      Lesson.all.each{|l| l.update_attribute :duration, 1*60*60 unless l.duration }
+    }
+
     add_column :courses, :end_at, :datetime
     Course.reset_column_information
     Course.all.each do |course|
