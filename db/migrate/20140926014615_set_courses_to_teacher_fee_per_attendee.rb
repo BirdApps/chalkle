b0c27a4b2457675e0853623c0f1b2c9f4d25f816
Matthew Kerr <matthew.kerr@me.com>
2014-10-11 20:43:08 +1300

    migration fixes

--- db/migrate/20140926014615_set_courses_to_teacher_fee_per_attendee.rb
+++ db/migrate/20140926014615_set_courses_to_teacher_fee_per_attendee.rb
@@ -1,7 +1,7 @@
 class SetCoursesToTeacherFeePerAttendee < ActiveRecord::Migration
   def up
     Course.all.each do |course|
-      course.update_attribute :teacher_pay_type, "Fee per attendee"
+      course.update_column :teacher_pay_type, "Fee per attendee"
     end
   end
 
