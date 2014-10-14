3dce48506919513af014f097804ae8362d155d00
Matthew Kerr <matthew.kerr@me.com>
2014-10-15 00:11:01 +1300

    db/migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
+
 ActiveRecord::Schema.define(:version => 20141016054518) do
 
   create_table "active_admin_comments", :force => true do |t|
@@ -185,7 +186,6 @@ ActiveRecord::Schema.define(:version => 20141016054518) do
   create_table "channel_plans", :force => true do |t|
     t.string   "name"
     t.integer  "max_channel_admins"
-    t.integer  "max_teachers"
     t.integer  "max_free_class_attendees"
     t.decimal  "class_attendee_cost"
     t.decimal  "course_attendee_cost"
@@ -193,6 +193,7 @@ ActiveRecord::Schema.define(:version => 20141016054518) do
     t.decimal  "processing_fee_percent"
     t.datetime "created_at",               :null => false
     t.datetime "updated_at",               :null => false
+    t.integer  "max_teachers"
   end
 
   create_table "channel_regions", :force => true do |t|
@@ -241,9 +242,9 @@ ActiveRecord::Schema.define(:version => 20141016054518) do
     t.decimal  "plan_course_attendee_cost"
     t.decimal  "plan_annual_cost"
     t.decimal  "plan_processing_fee_percent"
+    t.integer  "plan_max_teachers"
     t.string   "tax_number"
     t.string   "average_hero_color"
-    t.integer  "plan_max_teachers"
     t.decimal  "balance"
   end
 
