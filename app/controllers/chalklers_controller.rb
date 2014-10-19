9d84b411b75047162fe585b9fef87afbe13754da
Josh Dean <jdbdean@gmail.com>
2014-10-20 12:09:19 +1300

    fix permissions on reading/writing classes for admins and teacher

--- app/controllers/chalklers_controller.rb
+++ app/controllers/chalklers_controller.rb
@@ -10,6 +10,24 @@ class ChalklersController < ApplicationController
     @page_subtitle = '<a href="/people">Chalklers</a>'.html_safe
     @page_title = @chalkler.name
     @page_title_logo = @chalkler.avatar
+    @page_context_links = []
+    if current_user.super?
+     @page_context_links << {
+        img_name: "people",
+        link: becoming_sudo_chalklers_path(@chalkler.id),
+        active: false,
+        title: "Become"
+      }
+    end
+    if @chalkler == current_chalkler
+     @page_context_links << {
+        img_name: "settings",
+        link: me_preferences_path,
+        active: false,
+        title: "Settings"
+      }
+    end
+    
   end
 
   def exists
