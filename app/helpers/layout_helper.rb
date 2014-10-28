ba043232c3e6f2f6d24252d23eee021aff74d629
Josh Dean <jdbdean@gmail.com>
2014-10-29 10:36:21 +1300

    fixed search filter issues

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -78,7 +78,7 @@ module LayoutHelper
   end
 
   def filter_params(type, value, include_type = nil)
-    types = %w(region topic provider search take)
+    types = %w( topic region provider search take )
     types << include_type if include_type
     params_copy = Hash.new
     types.each do |type_i|
