4b22cad2d771a8ee9cb61fbde7f0305477d1ea16
Matthew Kerr <matthew.kerr@me.com>
2014-11-04 12:32:41 +1300

    more changes to the header

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -52,6 +52,7 @@ $(function(){
   var header_content_bg = $('body.v2 .header_content_bg');
   var filter_bar = $('.filter-nav');
   var coloring = $('.coloring');
+  scrolltop = function(){ return $(document).scrollTop(); };
 
   background_size_for_header_images = function(){
     var window_width = $(window).width();
@@ -60,25 +61,35 @@ $(function(){
 
   };
 
+  header_image_parallax = function(){
+    var header = $('.header');
+    header.css("background-position", 
+      ( 'center bottom ' + ((-scrolltop())/$(window).height()*200) + 'px' ) );
+    header_content_bg.css("background-position", 
+      ( 'center bottom ' + ((-scrolltop())/$(window).height()*200) + 'px' ) );
+  };
+
   fade_filterbar = function(){
-      var scrolltop = $(document).scrollTop();
       var hardtop = $('.navbar-fixed-top').height();
       var can_affect =  $(".filter-nav .dropdown.open").length == 0;
-      if(scrolltop > 300 && can_affect) {
-        filter_bar.css("top", ((-scrolltop+300)/85*30) +hardtop);
-        coloring.css( { "padding-top" : ((-scrolltop+300)/85*10) + 10 +"px" } );
-        coloring.css( { "padding-bottom" : ((-scrolltop+300)/85*10) + 10 +"px" } );
+      if(scrolltop() > 300 && can_affect) {
+        filter_bar.css("top", ((-scrolltop()+300)/85*30) +hardtop);
+        coloring.css( { "padding-top" : ((-scrolltop()+300)/85*10) + 10 +"px" } );
+        coloring.css( { "padding-bottom" : ((-scrolltop()+300)/85*10) + 10 +"px" } );
       } else {
         filter_bar.css("top", hardtop);
         coloring.css( { "padding-top" : '10px' } );
         coloring.css( { "padding-bottom" : '10px' } );
       };
-      if(scrolltop > 400 && can_affect) { 
+      if(scrolltop() > 400 && can_affect) { 
         filter_bar.css("display", 'none' );
+        coloring.css( { "padding-top" : '0' } );
+        coloring.css( { "padding-bottom" : '0' } );
+
       } else {
         filter_bar.css("display", 'block' );
         if (can_affect){
-          filter_bar.css("opacity", (-scrolltop+400)/65 ); 
+          filter_bar.css("opacity", (-scrolltop()+400)/65 ); 
         }else{
           filter_bar.css("opacity", 1); 
         }
@@ -91,5 +102,6 @@ $(function(){
   window.addEventListener("resize", background_size_for_header_images);
   window.addEventListener("resize", fade_filterbar);
   window.addEventListener("scroll", fade_filterbar);
+  window.addEventListener("scroll", header_image_parallax);
 
 });
