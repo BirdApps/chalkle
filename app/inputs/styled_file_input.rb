d1f08bd3c070f6a6a4b480c142e15b766be06e54
Josh Dean <jdbdean@gmail.com>
2014-11-20 13:32:56 +1300

    ability to customise text on styled file inptu

--- app/inputs/styled_file_input.rb
+++ app/inputs/styled_file_input.rb
@@ -1,10 +1,13 @@
 class StyledFileInput < SimpleForm::Inputs::Base
   def input
     input_html_options[:type] = :file
+    input_html_options[:content] = "Choose file" unless input_html_options[:content].present?
     "<div class='fakefilename'></div>
     <div class='styled-file'>
       #{ @builder.text_field(attribute_name, input_html_options)}
-      <div class='btn btn-primary fakebtn'>Choose file</div>
+      <div class='btn btn-primary fakebtn'>
+        #{ input_html_options[:content] }
+      </div>
     </div>
     ".html_safe
   end
