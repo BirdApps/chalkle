6d2c1d314fb3324045ba660dbc3028d4a4cb5f3d
Josh Dean <jdbdean@gmail.com>
2014-12-01 15:26:57 +1300

    clean up

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -21,22 +21,11 @@ class BookingsController < ApplicationController
   end
 
   def new
-    course = Course.find(params[:course_id])
-    @channel = course.channel #Find channel for hero
+    @channel = Course.find(params[:course_id]).channel #Find channel for hero
     @booking = Booking.new
     @booking.name = current_user.name unless @course.bookings_for(current_user).present?
     @page_subtitle = "Booking for"
     @page_title_logo = @course.course_upload_image if @course.course_upload_image.present?
-
-    interaction = {
-          actor:       current_chalkler,
-          target:      course,
-          action:      'new_booking',
-          controller:  'courses',
-          parameters:  params,
-          flag:        params[:flag]
-        }
-      Interaction.log interaction
   end
 
   def create
@@ -55,16 +44,6 @@ class BookingsController < ApplicationController
 
     # this should handle invalid @bookings before doing anything
     if @booking.save
-
-      interaction = {
-          actor:       current_chalkler,
-          target:      @booking.course,
-          action:      'create_booking',
-          controller:  'courses',
-          parameters:  params
-        }
-      Interaction.log interaction
-
       if @booking.free?
         BookingMailer.booking_confirmation(@booking).deliver!
         redirect_to @booking.course.path and return
@@ -184,7 +163,6 @@ class BookingsController < ApplicationController
   def load_booking
     @booking = current_user.bookings.find(params[:booking_id] || params[:id]).decorate
     redirect_to not_found and return if !@booking
-    interacted_with @booking
   end
 
   def redirect_on_paid
--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -155,7 +155,6 @@ class ChannelsController < ApplicationController
     if !@channel
       @channel = Channel.new(name: "All Providers")
     end
-    interacted_with @channel
   end
 
   def redirect_meetup_channels
