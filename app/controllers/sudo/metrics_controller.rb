7e5e240b89cc6eacc6e9765950cc7da7e80f37e9
Josh Dean <jdbdean@gmail.com>
2014-12-09 15:42:13 +1300

    sudo metrics uses new booking scope

--- app/controllers/sudo/metrics_controller.rb
+++ app/controllers/sudo/metrics_controller.rb
@@ -42,11 +42,11 @@ class Sudo::MetricsController < Sudo::BaseController
       }.reverse )
 
       f.series(:name => "Free", :yAxis => 0, :data => Array.new(30) {|i|
-        Booking.created_week_of(i.weeks.ago).confirmed.where{|b| b.payment==nil}.count
+        Booking.created_week_of(i.weeks.ago).confirmed.free.count
       }.reverse )
 
       f.series(:name => "Paid", :yAxis => 0, :data => Array.new(30) {|i|
-        Booking.created_week_of(i.weeks.ago).confirmed.where{|b| b.payment!=nil}.count
+        Booking.created_week_of(i.weeks.ago).confirmed.not_free.count
       }.reverse )
       f.legend(:align => 'center', :verticalAlign => 'bottom', :y => -30, :x => 0, :layout => 'horizontal')
       f.yAxis(max: 125)
