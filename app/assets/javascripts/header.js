00c8364a6ae078000e0ac252efa63c406cc78a0e
Josh Dean <jdbdean@gmail.com>
2014-12-09 18:57:33 +1300

    Revert "Revert "tidy up notifications load""
    This reverts commit 543500d7bb5a50e82e916700aa3edecc7c39b2af.

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -130,7 +130,7 @@ $(function(){
     });
 
     // update the title element
-    if(count<1) {
+    if(count>0) {
       $('title').html( "(" + count + ") " + ORIGINAL_TITLE); 
     } else { 
       $('title').html(ORIGINAL_TITLE); 
@@ -178,15 +178,17 @@ $(function(){
     update_notification_badge(notification_badge_count);
   };
 
-  var ORIGINAL_TITLE = $('title').html();
-  var NOTIFICATIONS_LOADED = false;
-
-  update_notification_list(true);
   background_size_for_header_images();
   fade_filterbar();
   check_notification_height();
 
-  //window.setInterval(update_notification_list, 7000);
+  var ORIGINAL_TITLE = $('title').html();
+  var NOTIFICATIONS_LOADED = false;
+
+  if($(".notifications-drop").length > 0){ 
+    update_notification_list(true);
+    window.setInterval(update_notification_list, 7000);
+  }
 
 
   coloring.click(big_color);
