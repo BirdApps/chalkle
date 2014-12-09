474ecb845ca5bf5def5801880a2112ca8cc03cee
Josh Dean <jdbdean@gmail.com>
2014-12-09 17:57:38 +1300

    sudo can view all notificaitons forever

--- app/controllers/sudo/chalklers_controller.rb
+++ app/controllers/sudo/chalklers_controller.rb
@@ -1,6 +1,9 @@
 class Sudo::ChalklersController < Sudo::BaseController
   before_filter :set_titles
 
+  def index
+  end
+
   def become
     sort_params = params[:order] || "name ASC"
     @chalklers = Chalkler.order(sort_params)
@@ -12,5 +15,10 @@ class Sudo::ChalklersController < Sudo::BaseController
     sign_in(:chalkler, Chalkler.find(params[:id]), { :bypass => true })
     redirect_to root_path
   end
+
+  def notifications
+    @notifications = Notification.by_date
+  end
+
 end
 
