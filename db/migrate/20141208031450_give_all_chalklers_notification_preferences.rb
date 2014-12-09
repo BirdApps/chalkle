bfa97708283f9a3782fc12149d072146aa5d3bdb
Josh Dean <jdbdean@gmail.com>
2014-12-09 18:08:52 +1300

    changes to route made migration fail

--- db/migrate/20141208031450_give_all_chalklers_notification_preferences.rb
+++ db/migrate/20141208031450_give_all_chalklers_notification_preferences.rb
@@ -6,7 +6,7 @@ class GiveAllChalklersNotificationPreferences < ActiveRecord::Migration
       Chalkler.scoped.each do |chalkler|
         unless chalkler.notification_preference.present?
           notifier = NotificationPreference.create chalkler: chalkler
-          chalkler.send_notification Notification::CHALKLE, me_preferences_notifications_path, "Hi! Notifications relevant to your activity on Chalkle will show up here from now on. We've also given you better control over what emails you would like to receive. **Email Preferences**"
+          chalkler.send_notification Notification::CHALKLE, me_notification_preference_path, "Hi! Notifications relevant to your activity on Chalkle will show up here from now on. We've also given you better control over what emails you would like to receive. **Email Preferences**"
         end
       end
     end
