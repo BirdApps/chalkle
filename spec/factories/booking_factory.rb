843b13774f8f5b0fb84afc8fb630b5f2e47f47db
Josh Dean <jdbdean@gmail.com>
2014-12-15 17:07:45 +1300

    modernised model factories

--- spec/factories/booking_factory.rb
+++ spec/factories/booking_factory.rb
@@ -1,20 +1,37 @@
 FactoryGirl.define do
   factory :booking do
-    name 'booker books'
+    name 'Joe Groot'
     status 'yes'
-    guests 3
     payment_method 'credit_card'
     payment {|i| i.association(:payment) }
     chalkler { |i| i.association(:chalkler)}
-    course { |i| i.association(:course)}
+    course { |i| i.association(:fee_per_attendee_course)}
+    
+    teacher_fee     8.50
+    teacher_gst     1.50
+    provider_fee     5.87
+    provider_gst     1.03
+    chalkle_fee     2.00
+    chalkle_gst     0.30
+    processing_fee  0.68
+    processing_gst  0.12
+
+
+    factory :cancelled_booking do
+      status 'no'
+    end
+
+    factory :pending_refund do
+      status 'refund_pending'
+    end
   end
-  factory :booking_unpaid do
-    name 'booker books'
+
+  factory :booking_free do
+    name 'Joe Groot'
     status 'yes'
-    guests 3
-    chalkle_fee 10
-    payment_method 'credit_card'
+    payment_method 'free'
     chalkler { |i| i.association(:chalkler)}
     course { |i| i.association(:course)}
   end
+
 end
