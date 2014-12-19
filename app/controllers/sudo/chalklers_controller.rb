d87d86477cf62035c8d07582398e6895566c713d
Matthew Kerr <matthew.kerr@me.com>
2014-12-19 13:50:38 +1300

    sudo chalklers fixed and specs added some refactoring of sudo menus.

--- app/controllers/sudo/chalklers_controller.rb
+++ app/controllers/sudo/chalklers_controller.rb
@@ -1,16 +1,12 @@
 class Sudo::ChalklersController < Sudo::BaseController
-  before_filter :set_titles
 
   def index
-  end
-
-  def become
     sort_params = params[:order] || "name ASC"
     @chalklers = Chalkler.order(sort_params)
 
   end
 
-  def becoming
+  def become
     return unless current_user.super?
     sign_in(:chalkler, Chalkler.find(params[:id]), { :bypass => true })
     redirect_to root_path
