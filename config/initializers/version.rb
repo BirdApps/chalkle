b1475b8e0b32abe8deab356454f7030bb33acc32
Matthew Kerr <matthew.kerr@me.com>
2015-01-21 12:45:21 +1300

    things change for new server setups

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,4 +1,4 @@
-tag = `git describe --exact-match --abbrev=0 --tags 2> /dev/null`.strip
-hash = `git log --pretty="%h" -1`.strip
+# tag = `git describe --exact-match --abbrev=0 --tags 2> /dev/null`.strip
+# hash = `git log --pretty="%h" -1`.strip
 
-VERSION_NUMBER = tag.blank? ? hash : tag
+# VERSION_NUMBER = tag.blank? ? hash : tag
