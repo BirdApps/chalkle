86c14ad0a0021078e4d348bbdbd78180dcb520c9
Matthew Kerr <matthew.kerr@me.com>
2015-01-22 14:28:11 +1300

    version for staging

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,2 +1,7 @@
-hash = `git log --pretty="%h" -1`.strip
-VERSION_NUMBER = `git log --pretty="%h" -1`.strip
+if Rails.env.development?
+  hash = `git log --pretty="%h" -1`.strip
+  VERSION_NUMBER = `git log --pretty="%h" -1`.strip
+else
+  hash = `cd ../repo && git log --pretty="%h" -1`.strip
+  VERSION_NUMBER = `cd ../repo && git log --pretty="%h" -1`.strip
+end
