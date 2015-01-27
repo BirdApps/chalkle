bf6de2a31224a6c2acb7be2963941f6e9bd0fa3b
Josh Dean <jdbdean@gmail.com>
2015-01-27 17:13:48 +1300

    cut out all the filler stuff now that we have real content to put in this page

--- app/assets/javascripts/v2/v2_booking_form.js
+++ app/assets/javascripts/v2/v2_booking_form.js
@@ -48,13 +48,5 @@ $(function(){
       }
     });
 
-    // $("#booking_guests").change(function(){
-    //   var friend_invite = $($('.booking_bookings')[0]).clone();
-    //   $('#friend_pen').html('<br />');
-    //   for(var i = 0; i < $(this).val(); i++){
-    //     $('#friend_pen br').before(friend_invite);
-    //   }
-    // })
-
   }
 });
