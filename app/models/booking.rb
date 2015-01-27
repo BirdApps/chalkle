adfec5d4bf9ed11f745a49c518ba586c201ceee8
Josh Dean <jdbdean@gmail.com>
2015-01-27 16:52:55 +1300

    compact vs compact! returns the object and nil respectively

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -263,9 +263,8 @@ class Booking < ActiveRecord::Base
   end
 
   def self.csv_for(bookings)
-    bookings.map { |b| }
     fields_for_csv = %w{ id name email paid note_to_teacher }
-    custom_fields_for_csv = bookings.map(&:custom_fields).map{|g| g.keys if g.is_a? Hash}.flatten.uniq.compact!
+    custom_fields_for_csv = bookings.map(&:custom_fields).map{|g| g.keys if g.is_a? Hash}.flatten.uniq.compact
     CSV.generate do |csv|
 
       headings = fields_for_csv.map(&:to_s)
