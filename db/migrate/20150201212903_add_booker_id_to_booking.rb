5a6d414ecf13f66a7910c147b475ace2f877b2a3
Matthew Kerr <matthew.kerr@me.com>
2015-02-02 19:49:26 +1300

    fix migration stuff

--- db/migrate/20150201212903_add_booker_id_to_booking.rb
+++ db/migrate/20150201212903_add_booker_id_to_booking.rb
@@ -2,7 +2,10 @@ class AddBookerIdToBooking < ActiveRecord::Migration
   def up
     add_column :bookings, :booker_id, :integer
     Booking.reset_column_information
-    puts Booking.all.map{ |b| b.update_column :booker_id, b.chalkler_id }.count+" bookings updated"
+    Booking.transaction do 
+      count =  Booking.all.map{ |b| b.update_column :booker_id, b.chalkler_id }.count 
+      puts "#{count} bookings updated"
+    end
   end
 
   def down
