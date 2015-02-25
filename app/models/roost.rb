3d5f8b1f17708e3628768131badc59d10bd04555
Matthew Kerr <matthew.kerr@me.com>
2015-02-25 21:28:13 +1300

    adds desktop notifications

---
+++ app/models/roost.rb
@@ -0,0 +1,26 @@
+class Roost
+  include HTTParty
+  attr_accessor :parameters
+
+  def initialize(initial_chalkler, message, url)
+    @parameters = { alert: message, url: url, alias: initial_chalkler.id }
+  end
+
+  def deliver
+    result = HTTParty.post("https://go.goroost.com/api/push", 
+      body: parameters.to_json, 
+      headers: { 'Content-Type' => 'application/json' }, 
+      basic_auth: keys
+    )
+    result
+  end
+
+  private
+  
+  def keys 
+    #username and passsword are the config and secret keys from Roost.com
+    {  username: "e7a09481b0d145b784c6b9dd5dd97cb0",
+       password: "bdc1164b79c94ca4afc2c443d91afb50" }
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(:version => 20150223044946) do
     t.text     "cancelled_reason"
     t.boolean  "reminder_mailer_sent",          :default => false
     t.boolean  "booking_completed_mailer_sent", :default => false
+    t.boolean  "chalkler_deleted",              :default => false
     t.text     "custom_fields"
     t.integer  "payment_id"
     t.string   "pseudo_chalkler_email"
@@ -230,8 +231,14 @@ ActiveRecord::Schema.define(:version => 20150223044946) do
 
   create_table "notification_preferences", :force => true do |t|
     t.integer  "chalkler_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                                          :null => false
+    t.datetime "updated_at",                                          :null => false
+    t.boolean  "chalkler_discussion_from_chalkler", :default => true
+    t.boolean  "chalkler_discussion_from_teacher",  :default => true
+    t.boolean  "teacher_bookings",                  :default => true
+    t.boolean  "teacher_discussion",                :default => true
+    t.boolean  "provider_bookings",                 :default => true
+    t.boolean  "provider_discussion",               :default => true
     t.text     "preferences"
   end
 
@@ -351,7 +358,6 @@ ActiveRecord::Schema.define(:version => 20150223044946) do
   create_table "provider_plans", :force => true do |t|
     t.string   "name"
     t.integer  "max_provider_admins"
-    t.integer  "max_teachers"
     t.integer  "max_free_class_attendees"
     t.decimal  "class_attendee_cost"
     t.decimal  "course_attendee_cost"
@@ -359,6 +365,7 @@ ActiveRecord::Schema.define(:version => 20150223044946) do
     t.decimal  "processing_fee_percent"
     t.datetime "created_at",               :null => false
     t.datetime "updated_at",               :null => false
+    t.integer  "max_teachers"
   end
 
   create_table "provider_teachers", :force => true do |t|
@@ -401,9 +408,9 @@ ActiveRecord::Schema.define(:version => 20150223044946) do
     t.decimal  "plan_course_attendee_cost"
     t.decimal  "plan_annual_cost"
     t.decimal  "plan_processing_fee_percent"
+    t.integer  "plan_max_teachers"
     t.string   "tax_number"
     t.string   "average_hero_color"
-    t.integer  "plan_max_teachers"
     t.decimal  "balance"
   end
 
