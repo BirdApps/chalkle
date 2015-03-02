1a75c0edc2d4927e0a28edcd2d0d215b6f838424
Matthew Kerr <matthew.kerr@me.com>
2015-03-02 14:17:59 +1300

    app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    session[:previous_url] || discover_path
+    session[:previous_url] || root_path
   end
   
   def styleguide
