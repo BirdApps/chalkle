d964d1d996c9cdb97cb4fe0bcdd9c91519899798
Matthew Kerr <matthew.kerr@me.com>
2015-03-02 16:55:48 +1300

    staging config

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -19,6 +19,14 @@ preload_app true
 
 before_fork do |server, worker|
 
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
