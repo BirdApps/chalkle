38dceb5f164f75bafcc53bbdba669ec8b1942d95
Matthew Kerr <matthew.kerr@me.com>
2015-03-09 11:29:05 +1300

    partner inquiry archived notification

--- app/mailers/partner_inquiry_mailer.rb
+++ app/mailers/partner_inquiry_mailer.rb
@@ -10,4 +10,12 @@ class PartnerInquiryMailer < BaseChalkleMailer
     end
   end
 
+  def archived_partner_inquiry(partner_inquiry, recipiant)
+    @partner_inquiry = partner_inquiry
+    mail(to: recipiant.email, subject: "New Partner inquiry on Chalkle") do |format| 
+      format.html { render layout: 'standard_mailer' }
+    end
+  end
+
+
 end
