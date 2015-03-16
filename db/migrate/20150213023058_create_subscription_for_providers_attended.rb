614f32c292c1b0f8a2331ef90522d77a85c0154c
Josh Dean <jdbdean@gmail.com>
2015-03-16 18:27:23 +1300

    changed method name

--- db/migrate/20150213023058_create_subscription_for_providers_attended.rb
+++ db/migrate/20150213023058_create_subscription_for_providers_attended.rb
@@ -2,7 +2,7 @@ class CreateSubscriptionForProvidersAttended < ActiveRecord::Migration
   def up
     Chalkler.scoped.each do |chalkler|
       chalkler.providers_attended.each do |provider|
-        unless chalkler.providers.include? provider
+        unless chalkler.providers_following.include? provider
           Subscription.create chalkler: chalkler, provider: provider
         end
       end
