043999ab2ed95242052617d276c33b7fb7a63976
Josh Dean <jdbdean@gmail.com>
2015-03-25 11:31:26 +1300

    fixed sudo to work on new layout

--- app/controllers/sudo/base_controller.rb
+++ app/controllers/sudo/base_controller.rb
@@ -1,8 +1,12 @@
 class Sudo::BaseController < ApplicationController
-  before_filter :authorize_super
+  before_filter :authorize_super, :sudo_header
   
   def authorize_super
     authorize :sudo, :super?
+  end
+
+  def sudo_header
+    @header_partial = '/layouts/headers/sudo'
   end
 
 end
