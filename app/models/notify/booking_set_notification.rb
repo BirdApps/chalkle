f3fce571e5c4c854ff548cfa841c1d42dcc516ae
Matthew Kerr <matthew.kerr@me.com>
2015-04-07 16:01:34 +1200

    booking_set notifications

--- app/models/notify/booking_set_notification.rb
+++ app/models/notify/booking_set_notification.rb
@@ -19,4 +19,43 @@ class Notify::BookingSetNotification < Notify::Notifier
     @booking_set.booker.send_notification Notification::REMINDER, declined_provider_course_bookings_path( @booking_set.course.path_params({ booking_ids: @booking_set.id }) ), message, @booking_set.course
   end
 
+
+
+  def confirmation
+
+    chalklers = @booking_set.bookings.collect(&:chalkler).uniq
+    bookings_grouped_by_chalkler = Hash.new
+    chalklers.each do |c|
+      bookings_grouped_by_chalkler[c] = @booking_set.bookings.select{|b| b.chalkler == c }
+    end
+    
+    
+    if booking.pseudo_chalkler_email
+
+      message = I18n.t('notify.booking.booked_in', course_name: booking.course.name, booker: booking.booker.name)
+      BookingMailer.booking_confirmation_to_non_chalkler(booking).deliver!
+      Chalkler.invite!({email: booking.pseudo_chalkler_email, name: booking.name}, booking.booker) if booking.invite_chalkler
+    
+    else
+
+      message = I18n.t('notify.booking.confirmation.to_chalkler', course_name: booking.course.name)
+      booking.chalkler.send_notification Notification::REMINDER, provider_course_path(booking.course.path_params), message, booking
+
+      if booking.chalkler.email_about? :booking_confirmation_to_chalkler
+        BookingMailer.booking_confirmation_to_chalkler(booking).deliver!  
+      end
+      
+    end
+
+    #to teacher
+    message = I18n.t('notify.booking.confirmation.to_teacher', course_name: booking.course.name, from_name: booking.name)
+
+    booking.teacher.chalkler.send_notification(Notification::REMINDER, provider_course_path(booking.course.path_params), message, booking) if booking.teacher.chalkler
+
+    if booking.teacher.chalkler.blank? || booking.teacher.chalkler.email_about?(:booking_confirmation_to_teacher)
+      BookingMailer.booking_confirmation_to_teacher(booking).deliver!
+    end
+  
+  end
+
 end
