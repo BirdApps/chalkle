99fc8ba5ae5c0a27f04e11bcbac0e3800dc6bb9f
Josh Dean <jdbdean@gmail.com>
2015-04-13 13:38:44 +1200

    style fixes

--- db/migrate/20150412224346_recalc_flat_fees.rb
+++ db/migrate/20150412224346_recalc_flat_fees.rb
@@ -1,8 +1,8 @@
 class RecalcFlatFees < ActiveRecord::Migration
   def up
     courses = Course.where(teacher_pay_type: Course.teacher_pay_types[0])
-    puts courses.map { |c| c.teacher_payment.recalculate!(true) if c.teacher_payment }
-    puts courses.map { |c| c.provider_payment.recalculate!(true) if c.provider_payment }
+    puts courses.map { |c| c.teacher_payment.recalculate!(true) if c.teacher_payment.present? }
+    puts courses.map { |c| c.provider_payment.recalculate!(true) if c.provider_payment.present? }
   end
 
   def down
