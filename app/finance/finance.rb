9ac3a4d5ff977a99a7056f928a1c921730817d66
Josh Dean <jdbdean@gmail.com>
2015-06-16 23:56:19 +1200

    finance module can remove sales tax also

--- app/finance/finance.rb
+++ app/finance/finance.rb
@@ -10,9 +10,23 @@ module Finance
       end
   end
 
+  def self.reverse_sales_tax_for(country_code)
+    case country_code
+      when :nz
+        3/23.to_f
+      else
+        0
+      end
+  end
+
   def self.apply_sales_tax_to(value, country_code = :nz)
     tax = self.sales_tax_for(country_code)
     value = value+value*tax
+  end
+
+  def self.remove_sales_tax_from(value, country_code = :nz)
+    negate_tax = self.reverse_sales_tax_for(country_code)
+    value = value-value*negate_tax
   end
 
   def self.payment_methods
