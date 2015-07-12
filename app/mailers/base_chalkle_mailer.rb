5b8dbcfea0a770751df91c84bc446034bfe7915a
Josh Dean <jdbdean@gmail.com>
2015-07-12 13:00:45 +1200

    wrapper in mailer base which returns if there is no email address

--- app/mailers/base_chalkle_mailer.rb
+++ app/mailers/base_chalkle_mailer.rb
@@ -2,5 +2,11 @@
 class BaseChalkleMailer < ActionMailer::Base
   helper CourseHelper #TODO fix the total weirdness of having the course helper here. Some kind of date lib probably needed
 
+  def mail(args)
+    # Fixes SMTP trying to send emails without email addresses
+    return if args[:to].nil?
+    super
+  end
+
 
 end
