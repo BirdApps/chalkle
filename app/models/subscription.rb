ded0ead22bd4a178d31c22f4b599492e27c24fa2
Josh Dean <jdbdean@gmail.com>
2015-12-08 14:21:48 +1300

    fix validation of subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,8 @@
 class Subscription < ActiveRecord::Base
   attr_accessible :chalkler_id, :provider_id, :chalkler, :provider, :pseudo_chalkler_email
-  validates_uniqueness_of :chalkler_id, :scope => :provider_id
+  validates_uniqueness_of :chalkler_id, :scope => :provider_id, allow_nil: true
+  validates_uniqueness_of :pseudo_chalkler_email, :scope => :provider_id, allow_nil: true
+  
   belongs_to :provider
   belongs_to :chalkler
 
