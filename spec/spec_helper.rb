0153cafd50c03741ce0eee2c45fe288d3114ac9a
Kiesia Carmine <hello@kiesia.com>
2016-09-09 19:43:26 +0700

    update spec helper for capybara webkit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,8 +18,9 @@ prefork = lambda {
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-  RSpec.configure do |config|
+  Capybara.javascript_driver = :webkit
 
+  RSpec.configure do |config|
     config.infer_spec_type_from_file_location!
     config.include(EmailSpec::Helpers)
     config.include(EmailSpec::Matchers)
@@ -46,9 +47,29 @@ prefork = lambda {
     #     --seed 1234
     config.order = "random"
 
+    config.before(:each, :js => true) do
+      DatabaseCleaner.strategy = :truncation
+    end
+
     config.before(:suite) do
-      DatabaseCleaner.strategy = :transaction
       DatabaseCleaner.clean_with(:truncation)
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.strategy = :transaction
+    end
+
+    config.before(:each, type: :feature) do
+      # :rack_test driver's Rack app under test shares database connection
+      # with the specs, so continue to use transaction strategy for speed.
+      driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
+
+      if !driver_shares_db_connection_with_specs
+        # Driver is probably for an external browser with an app
+        # under test that does *not* share a database connection with the
+        # specs, so use truncation strategy.
+        DatabaseCleaner.strategy = :truncation
+      end
     end
 
     config.before(:each) do
