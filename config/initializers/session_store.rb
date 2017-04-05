6adf9b16159326126ed4a531853967d0bfbd3f30
Genevieve Parkes <genevieve.parkes@gmail.com>
2017-04-05 23:05:48 +0200

    update session cookie name to invalidate all sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,7 +6,7 @@
 #   domain: :all,
 #   tld_length: ActionDispatch::Http::URL.tld_length + 1
 # )
-Chalkle::Application.config.session_store :active_record_store
+Chalkle::Application.config.session_store :cookie_store, key: '_change_me_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
